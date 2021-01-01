b7288c2e9038603cd881613fb68b53fce6e71d94
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-01 11:20:13 -0600

    Add getInput

--- easy/0001/go/main.go
+++ easy/0001/go/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+const resultFormatString string = "your name is %s, you are %s years old, and your username is %s"
 
 func main() {
-	fmt.Println("hello world")
+	age := getInput("Your age?", io.Reader(os.Stdin))
+	fmt.Printf(resultFormatString, nil, age, nil)
+}
+
+func getInput(prompt string, source io.Reader) string {
+	reader := bufio.NewReader(source)
+	fmt.Println(prompt)
+	input, err := reader.ReadString('\n')
+	if nil != err {
+		panic(err)
+	}
+	return strings.Replace(input, "\n", "", -1)
 }
