cbce4803b33c3b2966c88c1d268c7cc9569899fc
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-03 10:11:52 -0600

    Add rotateAlphabet functionality

--- easy/003/go/main.go
+++ easy/003/go/main.go
@@ -6,4 +6,10 @@ var alphabet string = "abcdefghijklmnopqrstuvwxyz"
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(rotateAlphabet(27))
+}
+
+func rotateAlphabet(places int) string {
+	shift := places % len(alphabet)
+	return alphabet[shift:] + alphabet[:shift]
 }
--- easy/003/go/main_test.go
+++ easy/003/go/main_test.go
@@ -13,8 +13,27 @@ func TestRootMain(t *testing.T) { TestingT(t) }
 
 type MainSuite struct {}
 
+type rotateAlphabetFixture struct {
+	places int
+	result string
+}
+
+var rotateAlphabetFixtures = []rotateAlphabetFixture{
+	{0, "abcdefghijklmnopqrstuvwxyz"},
+	{26, "abcdefghijklmnopqrstuvwxyz"},
+	{1, "bcdefghijklmnopqrstuvwxyza"},
+	{27, "bcdefghijklmnopqrstuvwxyza"},
+}
+
 var _ = Suite(&MainSuite{})
 
 func (s *MainSuite) TestMain(c *C) {
 
+}
+
+func (s *MainSuite) TestRotateAlphabet(c *C) {
+	for _, fixture := range rotateAlphabetFixtures {
+		result := rotateAlphabet(fixture.places)
+		c.Assert(result, Equals, fixture.result)
+	}
 }
