109fc8444a361a85d91a4d3694c04ab2f3488b26
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-04 15:11:21 -0600

    Add math tooling

--- easy/004/go/main.go
+++ easy/004/go/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
+
+const (
+	// Smallest ASCII character
+	asciiLowerBound int = 0
+	// Largest ASCII character
+	asciiUpperBound int = 255
+	// Smallest ASCII character we want (!)
+	characterLowerBound int = 33
+	// Largest ASCII character we want (~)
+	characterUpperBound int = 126
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("THIS IS NOT CRYPTOGRAPHICALLY SECURE")
+	fmt.Println(rand.Intn(1))
+}
+
+func min(numbers ...int) int {
+	smallest := numbers[0]
+	for _, number := range numbers {
+		if number < smallest {
+			smallest = number
+		}
+	}
+	return smallest
+}
+
+func max(numbers ...int) int {
+	largest := numbers[0]
+	for _, number := range numbers {
+		if number > largest {
+			largest = number
+		}
+	}
+	return largest
+}
+
+func randomIntInRange(min, max int) int {
+	return rand.Intn(max - min + 1) + min
 }
--- easy/004/go/main_test.go
+++ easy/004/go/main_test.go
@@ -15,6 +15,27 @@ type MainSuite struct {}
 
 var _ = Suite(&MainSuite{})
 
+const (
+	minFromIntRange int = 0
+	maxFromIntRage int = 10
+)
+
+var intRange = []int{1, 0, 10}
+
 func (s *MainSuite) TestMain(c *C) {
 
+}
+
+func (s *MainSuite) TestMin(c *C) {
+	minInt := min(intRange...)
+	c.Assert(minInt, Equals, minFromIntRange)
+}
+
+func (s *MainSuite) TestMax(c *C) {
+	maxInt := max(intRange...)
+	c.Assert(maxInt, Equals, maxFromIntRage)
+}
+
+func (s *MainSuite) TestRandomIntInRange(c *C) {
+	c.Assert(randomIntInRange(1, 1), Equals, 1)
 }
