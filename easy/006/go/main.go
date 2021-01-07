4df098f7ef84b12340beaf9b61d2e6752a6b34a7
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-06 18:34:37 -0600

    Add decimal precision lib for Pi to 30 digits

--- easy/006/go/main.go
+++ easy/006/go/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/shopspring/decimal"
+)
 
-const pi30Digits float64 = 3.141592653589793238462643383279
+var pi30Digits decimal.Decimal
 
 func main() {
+	pi30Digits, _ = decimal.NewFromString("3.141592653589793238462643383279")
 	fmt.Println("hello world")
 }
