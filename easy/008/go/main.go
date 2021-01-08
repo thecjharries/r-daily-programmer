2fbb2cbd5148a171fc6cbb8ae77041efb153d909
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-08 17:15:55 -0600

    Add lyric strings

--- easy/008/go/main.go
+++ easy/008/go/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// https://en.wikipedia.org/wiki/99_Bottles_of_Beer
+const (
+	nthLyric string = `%d bottles of beer on the wall, %d bottles of beer.
+Take one down, pass it around, %d bottles of beer on the wall...
+`
+	lastLyric string = `No more bottles of beer on the wall, no more bottles of beer.
+We've taken them down and passed them around; now we're drunk and passed out!
+`
+	errorBadBottleCount string = "Unable to use this bottleCount"
+)
+
 func main() {
 	fmt.Println("hello world")
 }
