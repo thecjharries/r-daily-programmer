7de16c66b5d010eeea3fba76a25033095992f911
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-10 09:31:02 -0600

    Pull in (very prescriptive!) patterns from the prompt

--- easy/010/go/main.go
+++ easy/010/go/main.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
+
+// These patterns come directly from the prompt
+// They're much too prescriptive and I don't like them.
+var allowedFormPatterns = []string{
+	`\d{9}`,
+	`\d{3}-\d{3}-\d{4}`,
+	`\d{3}\.\d{3}\.\d{4}`,
+	`\(\d{3}\)\s{0,1}\d{3}-\d{4}`,
+	`\d{3}\-\d{4}`,
+}
 
 func main() {
 	fmt.Println("hello world")
