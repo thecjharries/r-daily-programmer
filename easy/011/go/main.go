95ef94626081646ef7d54105cfbfe7e1f8b48751
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-11 17:34:13 -0600

    Link docs and create final fnc

--- easy/011/go/main.go
+++ easy/011/go/main.go
@@ -5,3 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 }
+
+// https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week#Disparate_variation
+func disparateGaussAlgorithm() int {
+	return 0
+}
