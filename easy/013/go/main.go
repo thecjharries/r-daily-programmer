84317bac63dcf9bf50edf6fc953fe477179ede3a
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-13 18:37:47 -0600

    Create slices with days in each month

--- easy/013/go/main.go
+++ easy/013/go/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+var normalDayCount = []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+var leapDayCount = []int{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+
 func main() {
 	fmt.Println("hello world")
 }
