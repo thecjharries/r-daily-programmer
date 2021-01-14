074d1ce8292143809f4a3362404751234ddb7414
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-14 17:44:22 -0600

    Copy Go boilerplate

---
+++ easy/014/go/main_test.go
@@ -0,0 +1,20 @@
+// Copyright 2020 CJ Harries
+// Licensed under http://www.apache.org/licenses/LICENSE-2.0
+
+package main
+
+import (
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+func TestRootMain(t *testing.T) { TestingT(t) }
+
+type MainSuite struct {}
+
+var _ = Suite(&MainSuite{})
+
+func (s *MainSuite) TestMain(c *C) {
+
+}
