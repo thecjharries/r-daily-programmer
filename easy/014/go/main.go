12206b6d015446b463469d6c864ab287a34f890c
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-14 18:02:36 -0600

    Create and test reverseSortByBlock

--- easy/014/go/main.go
+++ easy/014/go/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("hello world")
@@ -11,6 +14,15 @@ func reverseSort(slice []int) []int {
 	result := slice
 	for lead, tail := 0, len(result) - 1; lead < tail; lead, tail = lead + 1, tail -1 {
 		result[lead], result[tail] = result[tail], result[lead]
+	}
+	return result
+}
+
+func reverseSortByBlock(slice []int, blockSize int) []int {
+	var result []int
+	for index := 0; index < len(slice); index += blockSize {
+		sliceCap := int(math.Min(float64(index + blockSize), float64(len(slice))))
+		result = append(result, reverseSort(slice[index:sliceCap])...)
 	}
 	return result
 }
--- easy/014/go/main_test.go
+++ easy/014/go/main_test.go
@@ -22,3 +22,11 @@ func (s *MainSuite) TestMain(c *C) {
 func (s *MainSuite) TestReverseSort(c *C) {
 	c.Assert(reverseSort([]int{1, 2, 3}), DeepEquals, []int{3, 2, 1})
 }
+
+func (s *MainSuite) TestReverseSortByBlock(c *C) {
+	c.Assert(
+		reverseSortByBlock([]int{12, 24, 32, 44, 55, 66}, 2),
+		DeepEquals,
+		[]int{24, 12, 44, 32, 66, 55},
+	)
+}
