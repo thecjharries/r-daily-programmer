eb9547d4e0fbe64dfa4980ef69575316bd01cfb9
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-15 17:11:05 -0600

    Create and test readFileIntoStringSlice with specific data

--- easy/015/go/main.go
+++ easy/015/go/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("hello world")
+}
+
+// https://stackoverflow.com/a/16615559
+func readFileIntoStringSlice(filename string) []string {
+	var result []string
+	file, _ := os.Open(filename)
+	defer (func() { _ = file.Close() })()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		result = append(result, scanner.Text())
+	}
+	return result
 }
--- easy/015/go/main_test.go
+++ easy/015/go/main_test.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"path"
 	"testing"
 
 	. "gopkg.in/check.v1"
@@ -17,4 +18,10 @@ var _ = Suite(&MainSuite{})
 
 func (s *MainSuite) TestMain(c *C) {
 
+}
+
+func (s *MainSuite) TestReadFileIntoStringSlice(c *C) {
+	stringSlice := readFileIntoStringSlice(path.Join(".", "sample.txt"))
+	c.Assert(106 == len(stringSlice), Equals, true)
+	c.Assert(stringSlice[0], Equals, "https://www.w3.org/TR/PNG/iso_8859-1.txt")
 }
