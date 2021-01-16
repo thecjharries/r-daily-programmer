e96401452f17be2824d751d72820ebd1bef6ed04
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-16 10:04:25 -0600

    Create function to strip characters

--- easy/016/go/main.go
+++ easy/016/go/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
 
 func main() {
 	fmt.Println("hello world")
+}
+
+func removeCharactersFromString(charactersToRemove, input string) string {
+	charactersToRemovePattern := regexp.MustCompile(fmt.Sprintf("[%s]", charactersToRemove))
+	return charactersToRemovePattern.ReplaceAllString(input, "")
 }
