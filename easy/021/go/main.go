b2a689483a5727ef2b46defbe2ab23225de9c7b6
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-21 18:48:23 -0600

    Pull in code from the internet

--- easy/021/go/main.go
+++ easy/021/go/main.go
@@ -5,3 +5,31 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 }
+
+// Mostly from
+// https://www.golangprograms.com/golang-program-to-print-all-permutations-of-a-given-string.html
+func insertCharacterIntoEveryPositionOfString(existingString []rune, newCharacter rune) (permutations []string) {
+	for index := 0; index <= len(existingString); index++ {
+		permutations = append(
+			permutations,
+			string(existingString[:index]) + string(newCharacter) + string(existingString[index:]),
+		)
+	}
+	return
+}
+
+// Mostly from
+// https://www.golangprograms.com/golang-program-to-print-all-permutations-of-a-given-string.html
+func permuteString(remaining []rune, permutations []string) []string {
+	if 0 == len(remaining) {
+		return permutations
+	}
+	var newPermutations []string
+	for _, permutation := range permutations {
+		newPermutations = append(
+			newPermutations,
+			insertCharacterIntoEveryPositionOfString([]rune(permutation), remaining[0])...,
+		)
+	}
+	return permuteString(remaining[1:], newPermutations)
+}
