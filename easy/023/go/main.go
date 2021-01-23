82f22ec6bb0530a355e0dcb0ea6a60728b88f8e9
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-23 11:28:00 -0600

    Create type and stub method

--- easy/023/go/main.go
+++ easy/023/go/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type MixedSlice []interface{}
+
+func (s *MixedSlice) SplitInHalf() (firstHalf, secondHalf []interface{}) {
+	return
+}
+
 func main() {
 	fmt.Println("hello world")
 }
