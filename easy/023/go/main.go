943e5daa883546f9276979fa924d78b8c36fbc80
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-23 11:30:21 -0600

    Complete method

--- easy/023/go/main.go
+++ easy/023/go/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type MixedSlice []interface{}
 
-func (s *MixedSlice) SplitInHalf() (firstHalf, secondHalf []interface{}) {
-	return
+func (s *MixedSlice) SplitInHalf() ([]interface{}, []interface{}) {
+	halfLen := int(math.Ceil(float64(len(*s)) / 2))
+	return (*s)[:halfLen], (*s)[halfLen:]
 }
 
 func main() {
