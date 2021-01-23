74f053486ee2c51c2ad0878cf294d7b38577d882
CJ Harries <cj@wizardsoftheweb.pro>
2021-01-23 11:39:58 -0600

    Test MixedSlice.SplitInHalf

--- easy/023/go/main_test.go
+++ easy/023/go/main_test.go
@@ -18,3 +18,17 @@ var _ = Suite(&MainSuite{})
 func (s *MainSuite) TestMain(c *C) {
 
 }
+
+func (s *MainSuite) TestMixedSliceSplitInHalf(c *C) {
+	first := MixedSlice{1, 2, 3, 4}
+	firstResult := []MixedSlice{{1, 2}, {3, 4}}
+	returnedFirst, returnedSecond := first.SplitInHalf()
+	c.Assert(returnedFirst, DeepEquals, firstResult[0])
+	c.Assert(returnedSecond, DeepEquals, firstResult[1])
+	second := MixedSlice{"a", "b", "c"}
+	secondResult := []MixedSlice{{"a", "b"}, {"c"}}
+	returnedFirst, returnedSecond = second.SplitInHalf()
+	c.Assert(returnedFirst, DeepEquals, secondResult[0])
+	c.Assert(returnedSecond, DeepEquals, secondResult[1])
+
+}
