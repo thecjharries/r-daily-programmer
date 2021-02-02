c7bd0724f99e9dd3c0c8b46c12c895ece5722f38
CJ Harries <cj@wizardsoftheweb.pro>
2021-02-01 18:06:48 -0600

    Add NewRoll

--- easy/032/go/main.go
+++ easy/032/go/main.go
@@ -14,7 +14,11 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Roll int
 
@@ -98,10 +102,57 @@ var possibleRolls = [37]string{
 	"35",
 }
 
+var rollSpace = [37]Roll{
+	R00,
+	R0,
+	R1,
+	R2,
+	R3,
+	R4,
+	R5,
+	R6,
+	R7,
+	R8,
+	R9,
+	R10,
+	R11,
+	R12,
+	R13,
+	R14,
+	R15,
+	R16,
+	R17,
+	R18,
+	R19,
+	R20,
+	R21,
+	R22,
+	R23,
+	R24,
+	R25,
+	R26,
+	R27,
+	R28,
+	R29,
+	R30,
+	R31,
+	R32,
+	R33,
+	R34,
+	R35,
+}
+
 func (r *Roll) String() string {
 	return possibleRolls[*r]
 }
 
+func NewRoll() Roll {
+	return rollSpace[rand.Intn(R35 - R00) + R00]
+}
+
+var zSeed int64 = time.Now().UnixNano()
+
 func main() {
+	rand.Seed(zSeed)
 	fmt.Println("hello world")
 }
