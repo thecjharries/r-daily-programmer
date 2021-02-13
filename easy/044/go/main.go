18c670b086e2c2991efcb62a6bb3b44e1d488bef
CJ Harries <cj@wizardsoftheweb.pro>
2021-02-13 10:40:07 -0600

    Add prompt to main

--- easy/044/go/main.go
+++ easy/044/go/main.go
@@ -25,7 +25,8 @@ var sentenceStopPattern = regexp.MustCompile(`[.?!]`)
 var promptInput = "If it will feed nothing else, it will\nfeed my revenge. He hath disgrac'd me and hind'red me half a\nmillion; laugh'd at my losses, mock'd at my gains, scorned my\nnation, thwarted my bargains, cooled my friends, heated mine\nenemies. And what's his reason? I am a Jew. Hath not a Jew eyes?\nHath not a Jew hands, organs, dimensions, senses, affections,\npassions, fed with the same food, hurt with the same weapons,\nsubject to the same diseases, healed by the same means, warmed\nand cooled by the same winter and summer, as a Christian is? If\nyou prick us, do we not bleed? If you tickle us, do we not laugh?\nIf you poison us, do we not die? And if you wrong us, shall we\nnot revenge? If we are like you in the rest, we will resemble you\nin that. If a Jew wrong a Christian, what is his humility?\nRevenge. If a Christian wrong a Jew, what should his sufferance\nbe by Christian example? Why, revenge. The villainy you teach me\nI will execute; and it shall go hard but I will better the\ninstruction."
 
 func main() {
-	fmt.Println("hello world")
+	longestSentence, characterLength := completePrompt(promptInput)
+	fmt.Printf("The longet sentence, with %d characters, is as follows: %s", characterLength, longestSentence)
 }
 
 func sanitizeNewLines(input string) string {
