15534ae6b07ffd57efa30efe1fcce071301d2e0b
CJ Harries <cj@wizardsoftheweb.pro>
2021-02-16 17:21:48 -0600

    Add prompt runner to main

--- easy/047/go/main.go
+++ easy/047/go/main.go
@@ -22,7 +22,17 @@ import (
 const romanAlphabet string = "abcdefghijklmnopqrstuvwxyz"
 
 func main() {
-	fmt.Println("hello world")
+	encryptedString := `    Spzalu - zayhunl dvtlu sfpun pu wvukz kpzaypibapun zdvykz pz uv ihzpz mvy h
+    zfzalt vm nvclyutlua.  Zbwyltl leljbapcl wvdly klypclz myvt h thukhal myvt aol
+    thzzlz, uva myvt zvtl mhyjpjhs hxbhapj jlyltvuf. Fvb jhu'a lewlja av dplsk
+    zbwyltl leljbapcl wvdly qbza 'jhbzl zvtl dhalyf ahya aoyld h zdvyk ha fvb! P
+    tlhu, pm P dlua hyvbuk zhfpu' P dhz hu ltwlylyvy qbza iljhbzl zvtl tvpzalulk
+    ipua ohk sviilk h zjptpahy ha tl aolf'k wba tl hdhf!... Ho, huk uvd dl zll aol
+    cpvslujl puolylua pu aol zfzalt! Jvtl zll aol cpvslujl puolylua pu aol zfzalt!
+    Olsw! Olsw! P't ilpun ylwylzzlk!`
+	for index := 0; index < 26; index++ {
+		fmt.Printf("Decrypting with a shift of %d\n%s\n\n", index, decryptString(encryptedString, index))
+	}
 }
 
 func sanitizeShift(shift int) int {
