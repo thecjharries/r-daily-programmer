84fe4c75ef2bfef96538d8b5d65dc7e8d2c18ffe
CJ Harries <cj@wizardsoftheweb.pro>
2021-02-17 17:09:38 -0600

    Add prompt runner to main

--- easy/048/go/main.go
+++ easy/048/go/main.go
@@ -17,7 +17,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(evenToTheFrontOddToTheBack(input))
 }
 
 func evenToTheFrontOddToTheBack(input []int) []int {
