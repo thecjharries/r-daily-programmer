7c1daa52d0b56393e7ceb35e73dd484a7753bb77
CJ Harries <cj@wizardsoftheweb.pro>
2021-03-03 17:58:53 -0600

    Add prompt runner to main

--- easy/062/go/main.go
+++ easy/062/go/main.go
@@ -20,7 +20,10 @@ import (
 )
 
 func main() {
-	fmt.Println("hello world")
+	elements := []float64{18.1, 55.1, 91.2, 74.6, 73.0, 85.9, 73.9, 81.4, 87.1, 49.3, 88.8, 5.7, 26.3, 7.1, 58.2, 31.7, 5.8, 76.9, 16.5, 8.1, 48.3, 6.8, 92.4, 83.0, 19.6}
+	maxSum := 98.2
+	size := 3
+	fmt.Println(ullmansPuzzle(elements, maxSum, size))
 }
 
 func doesSubsetSumLessThanMax(maxSum float64, subset []float64) bool {
