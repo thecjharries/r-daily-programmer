9c20e75c1f4b5876daad1ad37668e54003cb1666
CJ Harries <cj@wizardsoftheweb.pro>
2021-04-22 17:11:06 -0500

    Test results with valid URL

--- easy/112/go/main_test.go
+++ easy/112/go/main_test.go
@@ -53,3 +53,14 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestParseUrlAndReturnParams(c *C) {
+	var input string
+	var output map[string]string
+	input = "http://en.wikipedia.org/w/index.php?title=Main_Page&action=edit"
+	output = map[string]string{
+		"title":  "Main_Page",
+		"action": "edit",
+	}
+	c.Assert(parseUrlAndReturnParams(input), DeepEquals, output)
+}
