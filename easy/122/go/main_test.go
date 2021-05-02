0c90902f040d18d5234fc93a14fa03d5e5168877
CJ Harries <cj@wizardsoftheweb.pro>
2021-05-02 17:11:39 -0500

    Test calculatePositiveHashUnder10

--- easy/122/go/main_test.go
+++ easy/122/go/main_test.go
@@ -53,3 +53,7 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestCalculatePositiveHashUnder10(c *C) {
+	c.Assert(calculatePositiveHashUnder10(31337), Equals, 8)
+}
