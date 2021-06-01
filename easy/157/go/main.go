2dcc6663aba941b1110f82d0ba4c34933d27e5e3
CJ Harries <cj@wizardsoftheweb.pro>
2021-06-01 18:45:11 -0500

    Define TicTacToe type

--- easy/157/go/main.go
+++ easy/157/go/main.go
@@ -16,7 +16,7 @@ package main
 
 import "fmt"
 
-var solutionIndices = [][]int{
+var solutionIndices = [][3]int{
 	{0, 1, 2},
 	{3, 4, 5},
 	{6, 7, 8},
@@ -26,6 +26,8 @@ var solutionIndices = [][]int{
 	{0, 4, 8},
 	{2, 4, 6},
 }
+
+type TicTacToe [3][3]int
 
 var zPrint = fmt.Println
 
