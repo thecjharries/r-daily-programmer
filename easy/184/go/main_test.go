7a8ea16e1a5b13378bb2420d00ff4cf25cc0d42c
CJ Harries <cj@wizardsoftheweb.pro>
2021-06-28 18:16:23 -0500

    Test SmartStack.Pop

--- easy/184/go/main_test.go
+++ easy/184/go/main_test.go
@@ -65,3 +65,15 @@ func (s *MainSuite) TestSmartStackPush(c *C) {
 	c.Assert(stack.Sorted, DeepEquals, []int{5, 10, 12, 15})
 	c.Assert(stack.Stack, DeepEquals, []int{10, 15, 12, 5})
 }
+
+func (s *MainSuite) TestSmartStackPop(c *C) {
+	stack := SmartStack{
+		Sorted: []int{5, 10, 12, 15},
+		Stack:  []int{10, 15, 12, 5},
+	}
+	element := stack.Pop()
+	c.Assert(element, Equals, 5)
+	c.Assert(stack.Sorted, DeepEquals, []int{10, 12, 15})
+	c.Assert(stack.Stack, DeepEquals, []int{10, 15, 12})
+
+}
