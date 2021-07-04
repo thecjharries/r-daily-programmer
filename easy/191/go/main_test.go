75a855deb824196d580745ff88378b5e5591ed3c
CJ Harries <cj@wizardsoftheweb.pro>
2021-07-04 11:45:40 -0500

    Test parseWords

--- easy/191/go/main_test.go
+++ easy/191/go/main_test.go
@@ -30,6 +30,8 @@ var _ = Suite(&MainSuite{})
 var printCallCount int
 var printSpyContents string
 
+const loremIpsum string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 func printSpy(a ...interface{}) (n int, err error) {
 	printSpyContents = fmt.Sprint(a...)
 	printCallCount++
@@ -52,4 +54,10 @@ func (s *MainSuite) TestMain(c *C) {
 	main()
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
+}
+
+func (s *MainSuite) TestParseWords(c *C) {
+	output := parseWords(loremIpsum)
+	c.Assert(len(output), Equals, 69)
+	c.Assert(output[0], Equals, "lorem")
 }
