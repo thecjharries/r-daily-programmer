a26d976291a86a91a0cddbcae1db573010e2ddb6
CJ Harries <cj@wizardsoftheweb.pro>
2021-07-12 17:30:57 -0500

    Test convertStringToBanner

--- easy/199/go/main_test.go
+++ easy/199/go/main_test.go
@@ -53,3 +53,8 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestConvertStringToBanner(c *C) {
+	c.Assert(convertStringToBanner("abc"), Equals, "\n\n\n")
+	c.Assert(convertStringToBanner("000000000"), Equals, " _  _  _  _  _  _  _  _  _ \n| || || || || || || || || |\n|_||_||_||_||_||_||_||_||_|\n                           ")
+}
