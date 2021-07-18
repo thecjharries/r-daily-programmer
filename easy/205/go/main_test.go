98818da61de234fcd92f6767119bd294ec1144ae
CJ Harries <cj@wizardsoftheweb.pro>
2021-07-18 11:54:05 -0500

    Create test cases

--- easy/205/go/main_test.go
+++ easy/205/go/main_test.go
@@ -17,6 +17,7 @@ package main
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	. "gopkg.in/check.v1"
 )
@@ -52,4 +53,41 @@ func (s *MainSuite) TestMain(c *C) {
 	main()
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
+}
+
+func (s *MainSuite) TestSimplifyDate(c *C) {
+	var first, second, output string
+	currentYear := time.Now().Year()
+	first = fmt.Sprintf("%d-07-01", currentYear)
+	second = fmt.Sprintf("%d-07-04", currentYear)
+	output = "July 1 - 4"
+	c.Assert(simplifyDateRange(first, second), Equals, output)
+	first = fmt.Sprintf("%d-12-01", currentYear)
+	second = fmt.Sprintf("%d-02-03", currentYear+1)
+	output = "December 1 - February 3"
+	c.Assert(simplifyDateRange(first, second), Equals, output)
+	first = fmt.Sprintf("%d-12-01", currentYear)
+	second = fmt.Sprintf("%d-02-03", currentYear+2)
+	output = fmt.Sprintf("December 1, %d - February 3, %d", currentYear, currentYear+2)
+	c.Assert(simplifyDateRange(first, second), Equals, output)
+	first = fmt.Sprintf("%d-03-01", currentYear+1)
+	second = fmt.Sprintf("%d-05-05", currentYear+1)
+	output = fmt.Sprintf("March 1 - May 5, %d", currentYear+1)
+	c.Assert(simplifyDateRange(first, second), Equals, output)
+	first = fmt.Sprintf("%d-01-01", currentYear+2)
+	second = fmt.Sprintf("%d-01-01", currentYear+2)
+	output = fmt.Sprintf("January 1, %d", currentYear+2)
+	c.Assert(simplifyDateRange(first, second), Equals, output)
+	first = fmt.Sprintf("%d-09-05", currentYear+7)
+	second = fmt.Sprintf("%d-09-04", currentYear+8)
+	output = fmt.Sprintf("September 5, %d - September 4, %d", currentYear+7, currentYear+8)
+	c.Assert(simplifyDateRange(first, second), Equals, output)
+	first = fmt.Sprintf("%d-04-01", currentYear)
+	second = fmt.Sprintf("%d-09-10", currentYear+5)
+	output = fmt.Sprintf("April 1, %d - September 10, %d", currentYear, currentYear+5)
+	c.Assert(simplifyDateRange(first, second), Equals, output)
+	first = fmt.Sprintf("%d-12-11", currentYear)
+	second = fmt.Sprintf("%d-12-11", currentYear+1)
+	output = fmt.Sprintf("December 11, %d - December 11, %d", currentYear, currentYear+1)
+	c.Assert(simplifyDateRange(first, second), Equals, output)
 }
