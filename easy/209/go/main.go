3f3e96ab7dd91b226bced8bc3c1f9b6f9a3d3d81
CJ Harries <cj@wizardsoftheweb.pro>
2021-07-22 17:57:16 -0500

    Fix buildFlair

--- easy/209/go/main.go
+++ easy/209/go/main.go
@@ -36,11 +36,12 @@ func getSortedValuesAndMap(input map[string]float64) (values []float64, valueKey
 	return
 }
 
-func buildFlairMap(times map[string]float64) (output string) {
+func buildFlair(times map[string]float64) (output string) {
 	values, valueKeyMap := getSortedValuesAndMap(times)
 	previousValue := 0.0
 	for _, value := range values {
 		output += fmt.Sprintf("%s: %d\n", valueKeyMap[value], int(math.Floor(60-(value-previousValue))))
+		previousValue = value
 	}
 	return
 }
