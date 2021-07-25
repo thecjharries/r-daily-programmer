009a47b38eed507b0d1af8379cf6d221d8281cbb
CJ Harries <cj@wizardsoftheweb.pro>
2021-07-25 13:57:33 -0500

    Test convertToRobbersLanguage

--- easy/212/go/main_test.go
+++ easy/212/go/main_test.go
@@ -53,3 +53,7 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestConvertToRobbersLanguage(c *C) {
+	c.Assert(convertToRobbersLanguage("Jag talar Rövarspråket!"), Equals, "Jojagog totalolaror Rorövovarorsospoproråkoketot!")
+}
