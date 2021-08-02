5a333da8d47411789200d1fac4e1645a1b18b9fd
CJ Harries <cj@wizardsoftheweb.pro>
2021-08-02 17:29:36 -0500

    Create fnc to organize word per prompt

--- easy/220/go/main.go
+++ easy/220/go/main.go
@@ -17,9 +17,13 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"sort"
+	"strings"
+	"unicode"
 )
 
 var whitespacePattern = regexp.MustCompile(`\s`)
+var notLettersPattern = regexp.MustCompile(`(?i)([^a-z])`)
 
 var zPrint = fmt.Println
 
@@ -29,4 +33,28 @@ func main() {
 
 func explodeOnWhitespace(input string) []string {
 	return whitespacePattern.Split(input, -1)
+}
+
+func organizeWord(input string) string {
+	// Find all the punctuation
+	punctuationIndices := notLettersPattern.FindAllStringSubmatchIndex(input, -1)
+	punctuationMatches := notLettersPattern.FindAllStringSubmatch(input, -1)
+	// Sort the letters and convert back to string
+	explodedOutput := strings.Split(strings.ToLower(notLettersPattern.ReplaceAllString(input, "")), "")
+	sort.Strings(explodedOutput)
+	lowerOutput := strings.Join(explodedOutput, "")
+	// Reinject punctuation
+	for matchIndex, indexGroup := range punctuationIndices {
+		for index := 2; index < len(indexGroup); index += 2 {
+			lowerOutput = fmt.Sprintf("%s%s%s", lowerOutput[:indexGroup[index]], punctuationMatches[matchIndex][index/2], lowerOutput[indexGroup[index]:])
+		}
+	}
+	// Fix capital letters
+	outputRune := []rune(lowerOutput)
+	for index, character := range input {
+		if unicode.IsUpper(character) {
+			outputRune[index] = unicode.ToUpper(outputRune[index])
+		}
+	}
+	return string(outputRune)
 }
