2e926eba508a331075e168f7e5d0098f294fb65d
CJ Harries <cj@wizardsoftheweb.pro>
2021-08-04 17:04:45 -0500

    Create fnc to get int value of letter

--- easy/222/go/main.go
+++ easy/222/go/main.go
@@ -14,10 +14,72 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 var zPrint = fmt.Println
 
 func main() {
 	_, _ = zPrint("hello world")
+}
+
+func getLetterValue(input rune) int {
+	switch unicode.ToLower(input) {
+	case 'a':
+		return 1
+	case 'b':
+		return 2
+	case 'c':
+		return 3
+	case 'd':
+		return 4
+	case 'e':
+		return 5
+	case 'f':
+		return 6
+	case 'g':
+		return 7
+	case 'h':
+		return 8
+	case 'i':
+		return 9
+	case 'j':
+		return 10
+	case 'k':
+		return 11
+	case 'l':
+		return 12
+	case 'm':
+		return 13
+	case 'n':
+		return 14
+	case 'o':
+		return 15
+	case 'p':
+		return 16
+	case 'q':
+		return 17
+	case 'r':
+		return 18
+	case 's':
+		return 19
+	case 't':
+		return 20
+	case 'u':
+		return 21
+	case 'v':
+		return 22
+	case 'w':
+		return 23
+	case 'x':
+		return 24
+	case 'y':
+		return 25
+	case 'z':
+		return 26
+	default:
+		return 0
+	}
 }
