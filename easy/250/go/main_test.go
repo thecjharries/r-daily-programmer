3c4e8702a78ab561ae9be62cd912ee39709fa7b2
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-01 17:20:28 -0500

    Test loadData

--- easy/250/go/main_test.go
+++ easy/250/go/main_test.go
@@ -53,3 +53,8 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestLoadData(c *C) {
+	posts := loadData()
+	c.Assert(len(posts), Equals, 25)
+}
