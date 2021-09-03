f5db1e4e9395fef034db37397843d12647283855
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-03 12:37:57 -0500

    Create a test for determinePoolSize

--- easy/252/go/main_test.go
+++ easy/252/go/main_test.go
@@ -53,3 +53,7 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestDeterminePoolSize(c *C) {
+	c.Assert(determinePoolSize(5), Equals, 3121)
+}
