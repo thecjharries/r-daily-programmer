fe4bf8a477d09f29f16de0e1ff87954dbef171c5
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-06 16:25:38 -0500

    Test NewSwitches

--- easy/255/go/main_test.go
+++ easy/255/go/main_test.go
@@ -53,3 +53,7 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestNewSwitches(c *C) {
+	c.Assert(len(NewSwitches(10)), Equals, 10)
+}
