cbdf62b99658da6802a3701b4ffb508a66762c22
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-07 18:06:03 -0500

    Test oblique

--- easy/256/go/main_test.go
+++ easy/256/go/main_test.go
@@ -53,3 +53,9 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestOblique(c *C) {
+	input := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35}
+	output := "0\n1 6\n2 7 12\n3 8 13 18\n4 9 14 19 24\n5 10 15 20 25 30\n11 16 21 26 31\n17 22 27 32\n23 28 33\n29 34\n35"
+	c.Assert(oblique(input), Equals, output)
+}
