b700b58e35dbe81889d83b2fdbba07c92d3be75a
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-07 19:18:40 -0500

    Flesh out oblique

--- easy/256/go/main.go
+++ easy/256/go/main.go
@@ -14,7 +14,12 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
 var zPrint = fmt.Println
 
@@ -23,5 +28,22 @@ func main() {
 }
 
 func oblique(input []int) (output string) {
+	size := int(math.Sqrt(float64(len(input))))
+	for diagonalRank := 0; diagonalRank < size; diagonalRank++ {
+		currentRow := make([]string, 0)
+		for columnIndex := 0; columnIndex <= diagonalRank; columnIndex++ {
+			currentRow = append(currentRow, strconv.Itoa(input[columnIndex+size*(diagonalRank-columnIndex)]))
+		}
+		output += fmt.Sprintf("%s\n", strings.Join(currentRow, " "))
+	}
+	offset := 0
+	for diagonalRank := size - 1; diagonalRank > 0; diagonalRank-- {
+		currentRow := make([]string, 0)
+		for rowIndex := size - diagonalRank; rowIndex < size; rowIndex++ {
+			currentRow = append(currentRow, strconv.Itoa(input[size-rowIndex+offset+size*rowIndex]))
+		}
+		offset++
+		output += fmt.Sprintf("%s\n", strings.Join(currentRow, " "))
+	}
 	return
 }
