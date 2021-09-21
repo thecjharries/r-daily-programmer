6f6e99c184f3debb68765023599bb72fb15af14c
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-21 17:36:04 -0500

    Test transposeText

--- easy/270/go/main_test.go
+++ easy/270/go/main_test.go
@@ -53,3 +53,8 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestTransposeText(c *C) {
+	c.Assert(transposeText("Some\ntext."), Equals, "St\noe\nmx\net\n .")
+	c.Assert(transposeText("package main\n\nimport \"fmt\"\n\nfunc main() {\n    queue := make(chan string, 2)\n    queue <- \"one\"\n    queue <- \"twoO\"\n    close(queue)\n    for elem := range queue {\n        fmt.Println(elem)\n    }\n}"), Equals, "p i f       }\na m u\nc p n\nk o c\na r  qqqcf }\ng t muuulo\ne   aeeeor\n  \" iuuus\nm f neeeeef\na m (   (lm\ni t ):<<qet\nn \"  =--um.\n    {   e P\n     m\"\"u:r\n     aote=i\n     knw) n\n     eeo rt\n     (\"O al\n     c \" nn\n     h   g(\n     a   ee\n     n    l\n         qe\n     s   um\n     t   e)\n     r   u\n     i   e\n     n\n     g   {\n     ,\n\n     2\n     )")
+}
