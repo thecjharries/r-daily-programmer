23ff9b8dbbab98a62e5a06fd418167ea578b30b6
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-23 17:11:24 -0500

    Test NewEnglishScrabbleTiles

--- easy/272/go/main_test.go
+++ easy/272/go/main_test.go
@@ -53,3 +53,8 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestNewEnglishScrabbleTiles(c *C) {
+	tiles := NewEnglishScrabbleTiles()
+	c.Assert(len(tiles), Equals, 27)
+}
