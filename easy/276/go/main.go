072892e025b4f6843ff209f1545f8c12d93dc2e5
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-27 20:13:14 -0500

    Flesh out printWordRectangle

--- easy/276/go/main.go
+++ easy/276/go/main.go
@@ -14,7 +14,11 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
 var zPrint = fmt.Println
 
@@ -22,6 +26,19 @@ func main() {
 	_, _ = zPrint("hello world")
 }
 
-func printWordRectangle(word string, width, height int) string {
-	return ""
+func printWordRectangle(word string, width, height int) (output string) {
+	end := strings.Split(word[1:len(word)-1], "")
+	sort.Sort(sort.Reverse(sort.StringSlice(end)))
+	searchSpace := word + strings.Join(end, "")
+	for y := 0; y < (len(word)-1)*height+1; y++ {
+		for x := 0; x < (len(word)-1)*width+1; x++ {
+			if 0 == x%(len(word)-1) || 0 == y%(len(word)-1) {
+				output += searchSpace[(x+y)%len(searchSpace) : (x+y)%len(searchSpace)+1]
+			} else {
+				output += " "
+			}
+		}
+		output += "\n"
+	}
+	return
 }
