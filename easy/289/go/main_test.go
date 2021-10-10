80f41cb45ff173ae85cfe0f35fe9592b990ab6fc
CJ Harries <cj@wizardsoftheweb.pro>
2021-10-10 12:53:28 -0500

    Fix fire/rock multiplier

--- easy/289/go/main_test.go
+++ easy/289/go/main_test.go
@@ -59,5 +59,5 @@ func (s *MainSuite) TestComputeDamageMultiplier(c *C) {
 	c.Assert(computeDamageMultiplier("fighting", []string{"ice", "rock"}), Equals, 4.0)
 	c.Assert(computeDamageMultiplier("psychic", []string{"poison", "dark"}), Equals, 0.0)
 	c.Assert(computeDamageMultiplier("water", []string{"normal"}), Equals, 1.0)
-	c.Assert(computeDamageMultiplier("fire", []string{"rock"}), Equals, 0.0)
+	c.Assert(computeDamageMultiplier("fire", []string{"rock"}), Equals, 0.5)
 }
