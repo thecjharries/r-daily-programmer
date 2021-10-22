062437a1454f130142861792a2b0037b55384042
CJ Harries <cj@wizardsoftheweb.pro>
2021-10-22 16:09:08 -0500

    Test NewJournal

--- easy/304/go/main_test.go
+++ easy/304/go/main_test.go
@@ -69,3 +69,11 @@ func (s *MainSuite) TestNewAccount(c *C) {
 	c.Assert(account.Name, Equals, "")
 	c.Assert(account.Balance, Equals, 0.0)
 }
+
+func (s *MainSuite) TestNewJournal(c *C) {
+	journal := NewJournal(
+		"1000;JAN-16;100000;0;\n3000;JAN-16;0;100000;\n7140;JAN-16;36000;0;\n1000;JAN-16;0;36000;\n1100;FEB-16;80000;0;\n1000;FEB-16;0;60000;\n2000;FEB-16;0;20000;\n1110;FEB-16;17600;0;\n2010;FEB-16;0;17600;\n1000;MAR-16;28500;0;\n4000;MAR-16;0;28500;\n2010;MAR-16;17600;0;\n1000;MAR-16;0;17600;\n5000;APR-16;19100;0;\n1000;APR-16;0;19100;\n1000;APR-16;32900;0;\n1020;APR-16;21200;0;\n4000;APR-16;0;54100;\n1000;MAY-16;15300;0;\n1020;MAY-16;0;15300;\n1000;MAY-16;4000;0;\n4090;MAY-16;0;4000;\n1110;JUN-16;5200;0;\n2010;JUN-16;0;5200;\n5100;JUN-16;19100;0;\n1000;JUN-16;0;19100;\n4120;JUN-16;5000;0;\n1000;JUN-16;0;5000;\n7160;JUL-16;2470;0;\n2010;JUL-16;0;2470;\n5500;JUL-16;3470;0;\n1000;JUL-16;0;3470;",
+		"1000;Cash;\n1020;Account Receivables;\n1100;Lab Equipement;\n1110;Office Supplies;\n2000;Notes Payables;\n2010;Account Payables;\n2110;Utilities Payables;\n3000;Common Stock;\n4000;Commercial Revenue;\n4090;Unearned Revenue;\n5000;Direct Labor;\n5100;Consultants;\n5500;Misc Costs;\n7140;Rent;\n7160;Telephone;\n9090;Dividends;",
+	)
+	c.Assert(journal[1000].Balance, Equals, 20430.0)
+}
