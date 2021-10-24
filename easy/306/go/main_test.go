c15af4ccba8147d3446d88a0e016c2b7cebba6d6
CJ Harries <cj@wizardsoftheweb.pro>
2021-10-24 11:32:36 -0500

    Test generateA105416

--- easy/306/go/main_test.go
+++ easy/306/go/main_test.go
@@ -53,3 +53,7 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestGenerateA105416(c *C) {
+	c.Assert(generateA105416(), Equals, []int{1444, 1446, 1464, 1466, 1644, 1646, 1664, 1666})
+}
