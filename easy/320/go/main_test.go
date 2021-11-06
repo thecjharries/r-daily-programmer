68327c65dd0f196ad37b835c90d85f8c79c390d5
CJ Harries <cj@wizardsoftheweb.pro>
2021-11-06 08:35:11 -0500

    Test buildNumberSpiral

--- easy/320/go/main_test.go
+++ easy/320/go/main_test.go
@@ -53,3 +53,20 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestBuildNumberSpiral(c *C) {
+	c.Assert(buildNumberSpiral(1), DeepEquals, [][]int{{1}})
+	c.Assert(buildNumberSpiral(5), DeepEquals, [][]int{
+		{1, 2, 3, 4, 5},
+		{16, 17, 18, 19, 6},
+		{15, 24, 25, 20, 7},
+		{14, 23, 22, 21, 8},
+		{13, 12, 11, 10, 9},
+	})
+	c.Assert(buildNumberSpiral(4), DeepEquals, [][]int{
+		{1, 2, 3, 4},
+		{12, 13, 14, 5},
+		{11, 16, 15, 6},
+		{10, 9, 8, 7},
+	})
+}
