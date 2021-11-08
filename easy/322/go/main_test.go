6eacaff454eb387fcec761e55893414f9fa03b54
CJ Harries <cj@wizardsoftheweb.pro>
2021-11-08 17:14:05 -0600

    Test generateAllPairsWeak

--- easy/322/go/main_test.go
+++ easy/322/go/main_test.go
@@ -53,3 +53,7 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestAllPairsWeak(c *C) {
+	c.Assert(generateAllPairsWeak([][]string{{"0", "1"}, {"A", "B", "C"}, {"D", "E", "F", "G"}}), Equals, 24)
+}
