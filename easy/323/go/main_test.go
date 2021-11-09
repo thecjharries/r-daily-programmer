d3065f451492800e3bf2bf755f4234bc9580ff62
CJ Harries <cj@wizardsoftheweb.pro>
2021-11-09 17:19:25 -0600

    Test threeSum

--- easy/323/go/main_test.go
+++ easy/323/go/main_test.go
@@ -53,3 +53,10 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestThreeSum(c *C) {
+	c.Assert(threeSum([]int{9, -6, -5, 9, 8, 3, -4, 8, 1, 7, -4, 9, -9, 1, 9, -9, 9, 4, -6, -8}), DeepEquals, [][]int{[]int{-5, -4, 9}, []int{-6, 3, 3}, []int{-5, 1, 4}, []int{-4, -4, 8}, []int{-9, 1, 8}, []int{-4, 1, 3}, []int{-8, 1, 7}, []int{-8, 4, 4}})
+	c.Assert(threeSum([]int{4, 5, -1, -2, -7, 2, -5, -3, -7, -3, 1}), DeepEquals, [][]int{[]int{-3, -1, 4}, []int{-2, -2, 4}, []int{-5, 1, 4}, []int{-3, -2, 5}, []int{-7, 2, 5}, []int{-1, -1, 2}, []int{-3, 1, 2}})
+	c.Assert(threeSum([]int{-1, -6, -3, -7, 5, -8, 2, -8, 1}), DeepEquals, [][]int{[]int{-1, -1, 2}, []int{-6, 1, 5}, []int{-3, 1, 2}, []int{-7, 2, 5}})
+	c.Assert(threeSum([]int{-5, -1, -4, 2, 9, -9, -6, -1, -7}), DeepEquals, [][]int{[]int{-5, -4, 9}, []int{-1, -1, 2}, []int{-4, 2, 2}})
+}
