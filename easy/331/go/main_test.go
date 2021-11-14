f7307699cb70f0f9f85ccd780372dd818d8b9962
CJ Harries <cj@wizardsoftheweb.pro>
2021-11-14 10:52:24 -0600

    Test isInteger

--- easy/331/go/main_test.go
+++ easy/331/go/main_test.go
@@ -53,3 +53,8 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestIsInteger(c *C) {
+	c.Assert(isInteger(1.0), Equals, true)
+	c.Assert(isInteger(1.1), Equals, false)
+}
