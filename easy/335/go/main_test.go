3068f39db1978e21ee8a540cf826458ea336a417
CJ Harries <cj@wizardsoftheweb.pro>
2021-11-16 17:58:57 -0600

    Test calculateConsecutiveDistanceRating

--- easy/335/go/main_test.go
+++ easy/335/go/main_test.go
@@ -53,3 +53,12 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestCalculateConsecutiveDistanceRating(c *C) {
+	c.Assert(calculateConsecutiveDistanceRating([]int{31, 63, 53, 56, 96, 62, 73, 25, 54, 55, 64}), Equals, 26)
+	c.Assert(calculateConsecutiveDistanceRating([]int{77, 39, 35, 38, 41, 42, 76, 73, 40, 31, 10}), Equals, 20)
+	c.Assert(calculateConsecutiveDistanceRating([]int{30, 63, 57, 87, 37, 31, 58, 83, 34, 76, 38}), Equals, 15)
+	c.Assert(calculateConsecutiveDistanceRating([]int{18, 62, 55, 92, 88, 57, 90, 10, 11, 96, 12}), Equals, 3)
+	c.Assert(calculateConsecutiveDistanceRating([]int{26, 8, 7, 25, 52, 17, 45, 64, 11, 35, 12}), Equals, 6)
+	c.Assert(calculateConsecutiveDistanceRating([]int{89, 57, 21, 55, 56, 81, 54, 100, 22, 62, 50}), Equals, 13)
+}
