0933a38abb090e3c1c52103032c879110cc60f4f
CJ Harries <cj@wizardsoftheweb.pro>
2021-11-18 14:28:16 -0600

    Test problem 1

--- easy/337/go/main_test.go
+++ easy/337/go/main_test.go
@@ -53,3 +53,7 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestProblem1(c *C) {
+	c.Assert(problem1(), Equals, 114.59155902616465)
+}
