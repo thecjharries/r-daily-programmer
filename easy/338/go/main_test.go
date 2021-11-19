a91aa9cd396d9d12fc05b75b8aa537cb316f3adb
CJ Harries <cj@wizardsoftheweb.pro>
2021-11-19 10:35:42 -0600

    Test findDayOfWeek

--- easy/338/go/main_test.go
+++ easy/338/go/main_test.go
@@ -53,3 +53,7 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestFindDayOfWeek(c *C) {
+	c.Assert(findDayOfWeek(2017, 10, 30), Equals, "Monday")
+}
