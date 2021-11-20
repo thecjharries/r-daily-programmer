c415963ac189561d9e809baa4273b31c09be5746
CJ Harries <cj@wizardsoftheweb.pro>
2021-11-20 09:53:33 -0600

    Test findHighestSalary

--- easy/339/go/main_test.go
+++ easy/339/go/main_test.go
@@ -53,3 +53,14 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestFindHighestSalary(c *C) {
+	c.Assert(findHighestSalary([]string{
+		"Boyce Calles        83460319",
+		"::EXT::SAL 00000000000044722",
+		"Marcelo Candela     29040821",
+		"::EXT::JOB loser            ",
+		"::EXT::SAL 00000000000047706",
+		"Milton Camper       32541106",
+	}), Equals, "Marcelo Candela, $47706")
+}
