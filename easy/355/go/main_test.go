3ed026d252325ad182a1074a40c8b248bdebfdc1
CJ Harries <cj@wizardsoftheweb.pro>
2021-12-05 11:21:51 -0600

    Test encodeLetter

--- easy/355/go/main_test.go
+++ easy/355/go/main_test.go
@@ -53,3 +53,9 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestEncodeLetter(c *C) {
+	c.Assert(encodeLetter('s', 't'), Equals, 'l')
+	c.Assert(encodeLetter('n', 'h'), Equals, 'u')
+	c.Assert(encodeLetter('i', 'e'), Equals, 'm')
+}
