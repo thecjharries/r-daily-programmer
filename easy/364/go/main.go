9935bf336abfbd67dbc1d0be043d34b19d7c6591
CJ Harries <cj@wizardsoftheweb.pro>
2021-12-10 17:57:44 -0600

    Use GH Copilot to write roll

--- easy/364/go/main.go
+++ easy/364/go/main.go
@@ -16,7 +16,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"regexp"
+	"strconv"
 )
 
 var dicePattern = regexp.MustCompile(`(\d+)d(\d+)`)
@@ -27,6 +29,16 @@ func main() {
 	_, _ = zPrint("hello world")
 }
 
-func rollDice(dice string) int {
-	return -1
+func roll(dice string) int {
+	matches := dicePattern.FindStringSubmatch(dice)
+	if len(matches) != 3 {
+		return 0
+	}
+	count, _ := strconv.Atoi(matches[1])
+	sides, _ := strconv.Atoi(matches[2])
+	sum := 0
+	for i := 0; i < count; i++ {
+		sum += 1 + rand.Intn(sides)
+	}
+	return sum
 }
