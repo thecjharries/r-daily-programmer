1f91b2b8bc56e364a59bb132178132ed15374b6f
CJ Harries <cj@wizardsoftheweb.pro>
2021-12-21 17:23:04 -0600

    Test hh

--- easy/378/go/main_test.go
+++ easy/378/go/main_test.go
@@ -53,3 +53,18 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestHh(c *C) {
+	c.Assert(hh([]int{5, 3, 0, 2, 6, 2, 0, 7, 2, 5}), Equals, false)
+	c.Assert(hh([]int{4, 2, 0, 1, 5, 0}), Equals, false)
+	c.Assert(hh([]int{3, 1, 2, 3, 1, 0}), Equals, true)
+	c.Assert(hh([]int{16, 9, 9, 15, 9, 7, 9, 11, 17, 11, 4, 9, 12, 14, 14, 12, 17, 0, 3, 16}), Equals, true)
+	c.Assert(hh([]int{14, 10, 17, 13, 4, 8, 6, 7, 13, 13, 17, 18, 8, 17, 2, 14, 6, 4, 7, 12}), Equals, true)
+	c.Assert(hh([]int{15, 18, 6, 13, 12, 4, 4, 14, 1, 6, 18, 2, 6, 16, 0, 9, 10, 7, 12, 3}), Equals, false)
+	c.Assert(hh([]int{6, 0, 10, 10, 10, 5, 8, 3, 0, 14, 16, 2, 13, 1, 2, 13, 6, 15, 5, 1}), Equals, false)
+	c.Assert(hh([]int{2, 2, 0}), Equals, false)
+	c.Assert(hh([]int{3, 2, 1}), Equals, false)
+	c.Assert(hh([]int{1, 1}), Equals, true)
+	c.Assert(hh([]int{1}), Equals, false)
+	c.Assert(hh([]int{}), Equals, true)
+}
