4d71809cc284e6b259707af4076847658a8f5ce5
CJ Harries <cj@wizardsoftheweb.pro>
2021-12-29 11:08:03 -0600

    Test change

--- easy/393/go/main_test.go
+++ easy/393/go/main_test.go
@@ -53,3 +53,10 @@ func (s *MainSuite) TestMain(c *C) {
 	c.Assert(printCallCount, Equals, 1)
 	c.Assert(printSpyContents, Equals, "hello world")
 }
+
+func (s *MainSuite) TestChange(c *C) {
+	c.Assert(change(0), Equals, 0)
+	c.Assert(change(12), Equals, 3)
+	c.Assert(change(468), Equals, 11)
+	c.Assert(change(123456), Equals, 254)
+}
