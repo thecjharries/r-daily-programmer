bffad3290883e86bd6589cc87e6f4bbbf4c64a38
CJ Harries <cj@wizardsoftheweb.pro>
2022-01-01 10:08:30 -0600

    Use Copilot to generate Rust for prompt

--- easy/399/rust/main.rs
+++ easy/399/rust/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn lettersum(s: &str) -> u32 {
+    s.chars().filter(|c| c.is_alphabetic()).map(|c| c.to_ascii_lowercase() as u32 - 'a' as u32 + 1).sum()
+}
