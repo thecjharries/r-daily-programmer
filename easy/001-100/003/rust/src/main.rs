da600a2d54b9cdb243c0336a59db65aa89abf183
CJ Harries <cj@wizardsoftheweb.pro>
2022-01-04 17:11:10 -0600

    Use Copilot to draft encrypt_caesar

--- easy/001-100/003/rust/src/main.rs
+++ easy/001-100/003/rust/src/main.rs
@@ -2,7 +2,25 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn encryptCaesar(input: &str, shift: u32) -> String {
+fn encrypt_caesar(input: &str, shift: u32) -> String {
     let mut output = String::new();
+    for input_character in input.chars() {
+        let mut character = input_character;
+        if character.is_alphabetic() {
+            let mut character_code = character as u8;
+            if character.is_lowercase() {
+                character_code = character_code + shift as u8;
+                if character_code > 'z' as u8 {
+                    character_code = character_code - 26;
+                }
+            } else {
+                character_code = character_code + shift as u8;
+                if character_code > 'Z' as u8 {
+                    character_code = character_code - 26;
+                }
+            }
+            character = character_code as char;
+        }
+    }
     output
 }
