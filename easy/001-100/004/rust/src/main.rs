d18dabb49b0847f443b6c474cbe55dbfb276c152
CJ Harries <cj@wizardsoftheweb.pro>
2022-01-05 17:48:57 -0600

    Fix the string builder

--- easy/001-100/004/rust/src/main.rs
+++ easy/001-100/004/rust/src/main.rs
@@ -7,7 +7,11 @@ fn main() {
 }
 
 fn random_password(rng: Pcg64,length: u32) -> String {
-    let password: String = rng.sample_iter(&Alphanumeric).take(length as usize).collect();
+    let password: String = rng
+        .sample_iter(&Alphanumeric)
+        .take(length as usize)
+        .map(char::from)
+        .collect();
     password
 }
 
