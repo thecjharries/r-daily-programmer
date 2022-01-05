5b2eac78f0711168bd1c3f989f849748535aa6fb
CJ Harries <cj@wizardsoftheweb.pro>
2022-01-05 17:47:57 -0600

    Test random_password

--- easy/001-100/004/rust/src/main.rs
+++ easy/001-100/004/rust/src/main.rs
@@ -1,3 +1,7 @@
+use rand::prelude::*;
+use rand::distributions::Alphanumeric;
+use rand_pcg::Pcg64;
+
 fn main() {
     println!("Hello, world!");
 }
@@ -6,4 +10,14 @@ fn random_password(rng: Pcg64,length: u32) -> String {
     let password: String = rng.sample_iter(&Alphanumeric).take(length as usize).collect();
     password
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_random_password() {
+        let mut rng = Pcg64::from_seed(0);
+        assert_eq!(random_password(rng, 10), "qqq");
+    }
 }
