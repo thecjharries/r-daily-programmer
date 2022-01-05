685f2df46e0e5a0c48eb3048df81c5879089fc34
CJ Harries <cj@wizardsoftheweb.pro>
2022-01-05 17:47:47 -0600

    Draft functionality for RNG password

--- easy/001-100/004/rust/src/main.rs
+++ easy/001-100/004/rust/src/main.rs
@@ -1,3 +1,9 @@
-fn random_password(length: u32) -> String {
-    String("test")
+fn main() {
+    println!("Hello, world!");
+}
+
+fn random_password(rng: Pcg64,length: u32) -> String {
+    let password: String = rng.sample_iter(&Alphanumeric).take(length as usize).collect();
+    password
+}
 }
