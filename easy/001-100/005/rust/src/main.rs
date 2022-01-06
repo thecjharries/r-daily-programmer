acdb9495e274589b0a2043004db9100c2b46fc0f
CJ Harries <cj@wizardsoftheweb.pro>
2022-01-06 07:07:28 -0600

    Test password_protected

--- easy/001-100/005/rust/src/main.rs
+++ easy/001-100/005/rust/src/main.rs
@@ -7,3 +7,16 @@ fn main() {
     let password = "hunter2";
     println!("{}", password_protected(username, password));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_password_protected() {
+        assert_eq!(password_protected("AzureDiamond", "hunter2"), true);
+        assert_eq!(password_protected("AzureDiamond", "hunter3"), false);
+        assert_eq!(password_protected("qqq", "hunter2"), false);
+        assert_eq!(password_protected("aaa", "hunter22"), false);
+    }
+}
