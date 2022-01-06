71bdc6a8436a060c374996020f7a9e5a859f0bf2
CJ Harries <cj@wizardsoftheweb.pro>
2022-01-06 07:06:20 -0600

    Use Copilot to build a main fnc

--- easy/001-100/005/rust/src/main.rs
+++ easy/001-100/005/rust/src/main.rs
@@ -1,3 +1,9 @@
 fn password_protected(username: &str, password: &str) -> bool {
     username == "AzureDiamond" && password == "hunter2"
 }
+
+fn main() {
+    let username = "AzureDiamond";
+    let password = "hunter2";
+    println!("{}", password_protected(username, password));
+}
