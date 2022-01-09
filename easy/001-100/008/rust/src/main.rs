548db908b5ac6252bac41ab4b018e45494c26c41
CJ Harries <cj@wizardsoftheweb.pro>
2022-01-09 13:09:53 -0600

    Test generate_lyrics

--- easy/001-100/008/rust/src/main.rs
+++ easy/001-100/008/rust/src/main.rs
@@ -29,3 +29,15 @@ fn generate_lyrics(bottles: i32) -> String {
     }
     lyrics
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_lyrics() {
+        assert_eq!(generate_lyrics(0), "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+        assert_eq!(generate_lyrics(1), "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+        assert_eq!(generate_lyrics(2), "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+    }
+}
