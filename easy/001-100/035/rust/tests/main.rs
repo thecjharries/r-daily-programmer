6f58231006d945dc9b8069efb14f57b1c1d93f5c
CJ Harries <cj@wizardsoftheweb.pro>
2022-02-04 09:29:26 -0600

    Update test location

--- easy/001-100/035/rust/tests/main.rs
+++ easy/001-100/035/rust/tests/main.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-fn main() {
-    println!("rad");
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_stub() {
+        assert_eq!(2 + 2, 4);
+    }
 }
