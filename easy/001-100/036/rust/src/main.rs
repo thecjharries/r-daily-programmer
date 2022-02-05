fff0c158f0ddc718b3a18707856018c3d66e6798
CJ Harries <cj@wizardsoftheweb.pro>
2022-02-05 07:31:43 -0600

    Test open_lockers

--- easy/001-100/036/rust/src/main.rs
+++ easy/001-100/036/rust/src/main.rs
@@ -26,6 +26,7 @@ mod tests {
 
     #[test]
     fn test_stub() {
-        assert_eq!(2 + 2, 4);
+        let open_lockers = vec![true, false, false, true, false, false, false, false, true];
+        assert_eq!(openLockers, open_lockers(openLockers.len()));
     }
 }
