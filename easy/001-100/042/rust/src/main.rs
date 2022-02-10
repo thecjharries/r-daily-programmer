5e4038ea93a37cc78dcc784b76585c8dbfa8171d
CJ Harries <cj@wizardsoftheweb.pro>
2022-02-10 08:51:27 -0600

    Pull in song

--- easy/001-100/042/rust/src/main.rs
+++ easy/001-100/042/rust/src/main.rs
@@ -12,6 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const SONG_BODY: String = "Old MACDONALD had a farm
+E-I-E-I-O
+And on his farm he had a {animal}s
+E-I-E-I-O
+With a {sound}s {sound}s here
+And a {sound}s {sound}s there
+Here a {sound}s, there a {sound}s
+Everywhere a {sound}s {sound}s
+Old MacDonald had a farm
+E-I-E-I-O
+
+".to_string();
+
 fn main() {
     println!("rad");
 }
