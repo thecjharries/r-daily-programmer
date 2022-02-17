5148bdb3f7e69694d4c9dd4dc78680216fa2b230
CJ Harries <cj@wizardsoftheweb.pro>
2022-02-17 17:30:45 -0600

    Double number of tests

--- easy/001-100/049/rust/src/main.rs
+++ easy/001-100/049/rust/src/main.rs
@@ -42,11 +42,17 @@ mod tests {
         assert_eq!(run_game_round(&mut rng, false), true);
         assert_eq!(run_game_round(&mut rng, false), true);
         assert_eq!(run_game_round(&mut rng, false), true);
+        assert_eq!(run_game_round(&mut rng, false), true);
+        assert_eq!(run_game_round(&mut rng, false), true);
+        assert_eq!(run_game_round(&mut rng, false), true);
     }
 
     #[test]
     fn test_run_game_round_player_switchess() {
         let mut rng = Pcg64::seed_from_u64(0);
+        assert_eq!(run_game_round(&mut rng, true), true);
+        assert_eq!(run_game_round(&mut rng, true), true);
+        assert_eq!(run_game_round(&mut rng, true), true);
         assert_eq!(run_game_round(&mut rng, true), true);
         assert_eq!(run_game_round(&mut rng, true), true);
         assert_eq!(run_game_round(&mut rng, true), true);
