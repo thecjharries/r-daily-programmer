1f879eadc99c8505cc9c2dc6c9a37c9ba2a81234
CJ Harries <cj@wizardsoftheweb.pro>
2022-03-15 17:30:27 -0500

    Build out logic to just pass the initial test (not a good exercise)

--- easy/001-100/075/rust/src/main.rs
+++ easy/001-100/075/rust/src/main.rs
@@ -12,12 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use regex::Regex;
+
 fn main() {
     println!("rad");
 }
 
 fn poorly_parse_c(input: String) -> String {
-    String::from("")
+    let exploded = input.split("=").collect::<Vec<&str>>();
+    let mut result = String::from("float ");
+    let lhs_pattern = Regex::new(r"(?P<name>[^(]+)\((?P<args>[^)]*)\)").unwrap();
+    let captured = lhs_pattern.captures(exploded[0]).unwrap();
+    result += &captured["name"];
+    result += "(";
+    let args = captured["args"]
+        .split(",")
+        .map(|arg| format!("float {}", arg))
+        .collect::<Vec<String>>();
+    result += &args.join(",");
+    result += ")";
+    result += "\n{\n    return ";
+    result += &exploded[1].replace("abs", "fabsf");
+    result += ";\n}";
+    result
 }
 
 #[cfg(test)]
