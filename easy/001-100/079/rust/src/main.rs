0a75d80e50ea386ae11f0813ac1507ea75cb8bcf
CJ Harries <cj@wizardsoftheweb.pro>
2022-03-19 11:06:22 -0500

    Test step_count

--- easy/001-100/079/rust/src/main.rs
+++ easy/001-100/079/rust/src/main.rs
@@ -25,7 +25,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_step_count() {
+        assert_eq!(
+            step_count(18.75, -22.00, 5),
+            vec![18.75, 8.5625, -1.625, -11.8125, -22.0]
+        );
+        assert_eq!(
+            step_count(-5.75, 12.00, 5),
+            vec![-5.75, -1.3125, 3.125, 7.5625, 12.0]
+        );
+        assert_eq!(step_count(13.50, -20.75, 3), vec![13.5, -3.625, -20.75]);
+        assert_eq!(
+            step_count(9.75, 3.00, 9),
+            vec![9.75, 8.90625, 8.0625, 7.21875, 6.375, 5.53125, 4.6875, 3.84375, 3.0]
+        );
     }
 }
