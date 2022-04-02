0c8283d190ee60962dc4f200a5c5fe9a0add3c4d
CJ Harries <cj@wizardsoftheweb.pro>
2022-04-02 12:02:15 -0500

    Fix types in MORSE_TO_ROMAN

--- easy/001-100/093/rust/src/main.rs
+++ easy/001-100/093/rust/src/main.rs
@@ -69,7 +69,7 @@ lazy_static! {
     static ref MORSE_TO_ROMAN: HashMap<&'static str, &'static str> = {
         let mut m = HashMap::new();
         for (k, v) in ROMAN_TO_MORSE.iter() {
-            m.insert(v, k);
+            m.insert(*v, *k);
         }
         m
     };
