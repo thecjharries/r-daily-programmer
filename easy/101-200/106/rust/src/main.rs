62374cdb29054cc0b1c475b9385a7eb8daf955ec
CJ Harries <cj@wizardsoftheweb.pro>
2022-04-15 17:27:33 -0500

    Add regex to match prompt word defn

--- easy/101-200/106/rust/src/main.rs
+++ easy/101-200/106/rust/src/main.rs
@@ -12,6 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref PROMPT_PATTERN: Regex =
+        Regex::new(r#"[\".,:;!?()[\]{}]|[^".,:;!?()[\]{}\s]+"#).unwrap();
+}
+
 fn main() {
     println!("rad");
 }
