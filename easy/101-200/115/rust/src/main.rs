7c57156f0961fd2939e0030126c7eda6b5df98b6
CJ Harries <cj@wizardsoftheweb.pro>
2022-04-24 17:41:33 -0500

    Use copilot and iterative testing to try to test the CLI actions

--- easy/101-200/115/rust/src/main.rs
+++ easy/101-200/115/rust/src/main.rs
@@ -14,13 +14,39 @@
 
 use rand::prelude::*;
 use rand_pcg::Pcg64;
-use std::io::{Error, Read, Write};
+use std::io::{BufRead, Read, Write};
 
 fn main() {
     println!("rad");
 }
 
-fn play_guessing_game<R: Rng>(rng: &mut R, input: &mut impl Read, output: &mut impl Write) {}
+fn play_guessing_game<R: Rng>(
+    rng: &mut R,
+    input: &mut (impl Read + BufRead),
+    output: &mut impl Write,
+) {
+    let mut guess = String::new();
+    let secret = rng.gen_range(1..101);
+    loop {
+        output
+            .write(b"Guess a number between 1 and 100: \n")
+            .unwrap();
+        output.flush().unwrap();
+        input.read_line(&mut guess).unwrap();
+        if guess.ends_with("exit\n") {
+            break;
+        }
+        let guess = guess.trim().parse::<u32>().unwrap();
+        if guess == secret {
+            output.write(b"You win!\n").unwrap();
+            break;
+        } else if guess < secret {
+            output.write(b"Too low!\n").unwrap();
+        } else {
+            output.write(b"Too high!\n").unwrap();
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,6 +54,27 @@ mod tests {
 
     #[test]
     fn test_play_guessing_game() {
-        assert_eq!(2 + 2, 4);
+        let mut rng = Pcg64::seed_from_u64(0);
+        let mut output: Vec<u8> = Vec::new();
+        play_guessing_game(&mut rng, &mut "50\nexit\n".as_bytes(), &mut output);
+        assert_eq!(
+            output,
+            b"Guess a number between 1 and 100: \nToo low!\nGuess a number between 1 and 100: \n"
+        );
+        output.clear();
+        play_guessing_game(&mut rng, &mut "50\nexit\n".as_bytes(), &mut output);
+        assert_eq!(
+            output,
+            b"Guess a number between 1 and 100: \nToo high!\nGuess a number between 1 and 100: \n"
+        );
+        output.clear();
+        play_guessing_game(&mut rng, &mut "50\nexit\n".as_bytes(), &mut output);
+        assert_eq!(
+            output,
+            b"Guess a number between 1 and 100: \nToo high!\nGuess a number between 1 and 100: \n"
+        );
+        output.clear();
+        play_guessing_game(&mut rng, &mut "exit\n".as_bytes(), &mut output);
+        assert_eq!(output, b"Guess a number between 1 and 100: \n");
     }
 }
