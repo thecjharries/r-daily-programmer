39bebc196b85b0ed53f3af4633b4d2c15af0acf0
CJ Harries <cj@wizardsoftheweb.pro>
2022-05-06 17:29:31 -0500

    Test mcarthy_91

--- easy/101-200/127/rust/src/main.rs
+++ easy/101-200/127/rust/src/main.rs
@@ -27,7 +27,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_mcarthy_91() {
+        let mut output: Vec<u8> = Vec::new();
+        assert_eq!(mcarthy_91(99, &mut output), 91);
+        assert_eq!(&output, b"M(M(110)) since 99 is equal to or less than 100\nM(100) since 110 is greater than 100\nM(M(111)) since 100 is equal to or less than 100\nM(101) since 111 is greater than 100\n91 since 101 is greater than 100\n");
+        output.clear();
+        assert_eq!(mcarthy_91(101, &mut output), 91);
+        assert_eq!(&output, b"91 since 101 is greater than 100\n");
+        output.clear();
+        assert_eq!(mcarthy_91(91, &mut output), 91);
+        assert_eq!(&output, b"M(M(102)) since 91 is equal to or less than 100\nM(92) since 102 is greater than 100\nM(M(103)) since 92 is equal to or less than 100\nM(93) since 103 is greater than 100\nM(M(104)) since 93 is equal to or less than 100\nM(94) since 104 is greater than 100\nM(M(105)) since 94 is equal to or less than 100\nM(95) since 105 is greater than 100\nM(M(106)) since 95 is equal to or less than 100\nM(96) since 106 is greater than 100\nM(M(107)) since 96 is equal to or less than 100\nM(97) since 107 is greater than 100\nM(M(108)) since 97 is equal to or less than 100\nM(98) since 108 is greater than 100\nM(M(109)) since 98 is equal to or less than 100\nM(99) since 109 is greater than 100\nM(M(110)) since 99 is equal to or less than 100\nM(100) since 110 is greater than 100\nM(M(111)) since 100 is equal to or less than 100\nM(101) since 111 is greater than 100\n91 since 101 is greater than 100\n");
+        output.clear();
+        assert_eq!(mcarthy_91(102, &mut output), 92);
+        assert_eq!(&output, b"M(92) since 102 is greater than 100\n");
     }
 }
