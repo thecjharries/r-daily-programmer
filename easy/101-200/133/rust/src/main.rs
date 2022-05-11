30f5e4db82335300c223e11622c154989d292757
CJ Harries <cj@wizardsoftheweb.pro>
2022-05-11 18:41:54 -0500

    Add new test

--- easy/101-200/133/rust/src/main.rs
+++ easy/101-200/133/rust/src/main.rs
@@ -51,7 +51,7 @@ mod tests {
 
     #[test]
     fn test_process_room_log() {
-        let log = vec![
+        let mut log = vec![
             (0, 0, 'I', 540),
             (1, 0, 'I', 540),
             (0, 0, 'O', 560),
@@ -63,6 +63,63 @@ mod tests {
                 "Room 0, 20 minute average visit, 1 visitor(s) total",
                 "Room 1, 20 minute average visit, 1 visitor(s) total"
             ]
+        );
+        log = vec![
+            (0, 11, 'I', 347),
+            (1, 13, 'I', 307),
+            (2, 15, 'I', 334),
+            (3, 6, 'I', 334),
+            (4, 9, 'I', 334),
+            (5, 2, 'I', 334),
+            (6, 2, 'I', 334),
+            (7, 11, 'I', 334),
+            (8, 1, 'I', 334),
+            (0, 11, 'O', 376),
+            (1, 13, 'O', 321),
+            (2, 15, 'O', 389),
+            (3, 6, 'O', 412),
+            (4, 9, 'O', 418),
+            (5, 2, 'O', 414),
+            (6, 2, 'O', 349),
+            (7, 11, 'O', 418),
+            (8, 1, 'O', 418),
+            (0, 12, 'I', 437),
+            (1, 28, 'I', 343),
+            (2, 32, 'I', 408),
+            (3, 15, 'I', 458),
+            (4, 18, 'I', 424),
+            (5, 26, 'I', 442),
+            (6, 7, 'I', 435),
+            (7, 19, 'I', 456),
+            (8, 19, 'I', 450),
+            (0, 12, 'O', 455),
+            (1, 28, 'O', 374),
+            (2, 32, 'O', 495),
+            (3, 15, 'O', 462),
+            (4, 18, 'O', 500),
+            (5, 26, 'O', 479),
+            (6, 7, 'O', 493),
+            (7, 19, 'O', 471),
+            (8, 19, 'O', 458),
+        ];
+        assert_eq!(
+            process_room_log(log),
+            vec![
+                "Room 1, 85 minute average visit, 1 visitor total",
+                "Room 2, 48 minute average visit, 2 visitors total",
+                "Room 6, 79 minute average visit, 1 visitor total",
+                "Room 7, 59 minute average visit, 1 visitor total",
+                "Room 9, 85 minute average visit, 1 visitor total",
+                "Room 11, 57 minute average visit, 2 visitors total",
+                "Room 12, 19 minute average visit, 1 visitor total",
+                "Room 13, 15 minute average visit, 1 visitor total",
+                "Room 15, 30 minute average visit, 2 visitors total",
+                "Room 18, 77 minute average visit, 1 visitor total",
+                "Room 19, 12 minute average visit, 2 visitors total",
+                "Room 26, 38 minute average visit, 1 visitor total",
+                "Room 28, 32 minute average visit, 1 visitor total",
+                "Room 32, 88 minute average visit, 1 visitor total",
+            ],
         );
     }
 }
