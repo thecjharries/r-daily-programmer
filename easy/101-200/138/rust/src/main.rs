8dd8c11f54f63cfea33128cc7a02d42d26648458
CJ Harries <cj@wizardsoftheweb.pro>
2022-05-16 17:15:28 -0500

    Create Particle struct

--- easy/101-200/138/rust/src/main.rs
+++ easy/101-200/138/rust/src/main.rs
@@ -12,6 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+struct Particle {
+    mass: f64,
+    position: (f64, f64),
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
