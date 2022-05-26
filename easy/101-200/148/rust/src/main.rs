9d9f030a30984bad00a13be5bcb012b930058458
CJ Harries <cj@wizardsoftheweb.pro>
2022-05-26 17:35:38 -0500

    Test count_rotation_increments

--- easy/101-200/148/rust/src/main.rs
+++ easy/101-200/148/rust/src/main.rs
@@ -31,7 +31,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_count_rotation_increments() {
+        assert_eq!(count_rotation_increments(5, 1, 2, 3), 21);
     }
 }
