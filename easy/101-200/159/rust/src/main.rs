066a80d05ca63d7b1788f2c990a8ee57c3b2f1dd
CJ Harries <cj@wizardsoftheweb.pro>
2022-06-01 17:15:39 -0500

    Add lazy_static for globals

--- easy/101-200/159/rust/src/main.rs
+++ easy/101-200/159/rust/src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use lazy_static::lazy_static;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
