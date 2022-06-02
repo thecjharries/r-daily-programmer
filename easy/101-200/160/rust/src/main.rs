c80f786723a99a336e7839036b46713a690af958
CJ Harries <cj@wizardsoftheweb.pro>
2022-06-02 18:05:53 -0500

    Test compute_angles

--- easy/101-200/160/rust/src/main.rs
+++ easy/101-200/160/rust/src/main.rs
@@ -26,7 +26,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_compute_angles() {
+        assert_eq!((36.87, 53.13, 90.0), compute_angles(3.0, 4.0, 5.0));
     }
 }
