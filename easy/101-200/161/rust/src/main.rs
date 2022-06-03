804f1d3dd62dc207425b49b3671459b8384f3a08
CJ Harries <cj@wizardsoftheweb.pro>
2022-06-03 17:28:52 -0500

    Pull in RNG deps

--- easy/101-200/161/rust/src/main.rs
+++ easy/101-200/161/rust/src/main.rs
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use rand::prelude::*;
+use rand::Rng;
+use rand_pcg::Pcg64;
+
 enum CardSuit {
     Clubs,
     Diamonds,
