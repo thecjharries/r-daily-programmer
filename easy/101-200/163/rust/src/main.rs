30a09c38c618bf9e097c0920172a6ca0875bf7b2
CJ Harries <cj@wizardsoftheweb.pro>
2022-06-05 10:20:29 -0500

    Test roll_and_build_stats

--- easy/101-200/163/rust/src/main.rs
+++ easy/101-200/163/rust/src/main.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use rand::prelude::SeedableRng;
 use rand::Rng;
 use rand_pcg::Pcg64;
 
@@ -29,7 +30,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_roll_and_build_stats() {
+        let mut rng = Pcg64::seed_from_u64(0);
+        assert_eq!(
+            "10         50.00% 10.00% 10.00% 00.00% 30.00% 00.00%",
+            roll_and_build_stats(10, &mut rng)
+        );
+        assert_eq!(
+            "100        21.00% 13.00% 16.00% 18.00% 16.00% 16.00%",
+            roll_and_build_stats(100, &mut rng)
+        );
+        assert_eq!(
+            "1000       17.00% 17.80% 14.60% 16.80% 16.10% 17.70%",
+            roll_and_build_stats(1000, &mut rng)
+        );
+        assert_eq!(
+            "10000      17.14% 17.16% 16.23% 16.16% 16.22% 17.09%",
+            roll_and_build_stats(10000, &mut rng)
+        );
     }
 }
