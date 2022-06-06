c558e9df92ceb05c954703eeca96c2dfba4780e6
CJ Harries <cj@wizardsoftheweb.pro>
2022-06-06 17:25:13 -0500

    Fix imports

--- easy/101-200/164/rust/src/main.rs
+++ easy/101-200/164/rust/src/main.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::io::{Error, Read, Result, Write};
+use std::io::{Error, Write};
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
