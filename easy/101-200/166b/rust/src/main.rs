2b8616fae7bdcf6002d5966e0380b97e4151793a
CJ Harries <cj@wizardsoftheweb.pro>
2022-06-08 17:31:31 -0500

    Define GRAVITATIONAL_CONSTANT

--- easy/101-200/166b/rust/src/main.rs
+++ easy/101-200/166b/rust/src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const GRAVITATIONAL_CONSTANT: f64 = 6.67e-11;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
