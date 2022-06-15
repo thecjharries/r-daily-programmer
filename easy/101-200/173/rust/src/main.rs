5c776c2d611a956b57a572f3288321d8b9d776b5
CJ Harries <cj@wizardsoftheweb.pro>
2022-06-15 07:05:39 -0500

    Define the conversion phrase pattern

--- easy/101-200/173/rust/src/main.rs
+++ easy/101-200/173/rust/src/main.rs
@@ -12,6 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref CONVERSION_PATTERN: Regex =
+        Regex::new(r"\s*(?P<amount>[^ ]*) (?P<from>.*) to (?P<to>.*)\s*").unwrap();
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
