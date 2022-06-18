0e7e72f524e8cc584d86889391e6033e24058a7d
CJ Harries <cj@wizardsoftheweb.pro>
2022-06-18 12:50:41 -0500

    Test determine_cells

--- easy/101-200/176/rust/src/main.rs
+++ easy/101-200/176/rust/src/main.rs
@@ -35,7 +35,42 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_determine_cells() {
+        let cells = determine_cells("B1:B3&B4:E10&F1:G1&F4~C5:C8&B2");
+        assert_eq!(29, cells.len());
+        assert_eq!(
+            HashSet::from_iter([
+                (1, 0),
+                (1, 2),
+                (1, 3),
+                (1, 4),
+                (1, 5),
+                (1, 6),
+                (1, 7),
+                (1, 8),
+                (1, 9),
+                (2, 3),
+                (2, 8),
+                (2, 9),
+                (3, 3),
+                (3, 4),
+                (3, 5),
+                (3, 6),
+                (3, 7),
+                (3, 8),
+                (3, 9),
+                (4, 3),
+                (4, 4),
+                (4, 5),
+                (4, 6),
+                (4, 7),
+                (4, 8),
+                (4, 9),
+                (5, 0),
+                (6, 0),
+                (5, 3),
+            ]),
+            cells,
+        );
     }
 }
