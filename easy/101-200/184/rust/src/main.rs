af9593f4f2df9220c3433255e7a6020f8b9759f9
CJ Harries <cj@wizardsoftheweb.pro>
2022-06-26 11:00:41 -0500

    Test SmartStack::push

--- easy/101-200/184/rust/src/main.rs
+++ easy/101-200/184/rust/src/main.rs
@@ -47,4 +47,15 @@ mod tests {
         assert_eq!(stack.stack.len(), 0);
         assert_eq!(stack.ordered.len(), 0);
     }
+
+    #[test]
+    fn test_smartstack_push() {
+        let mut stack = SmartStack {
+            stack: vec![3, 2, 1],
+            ordered: vec![1, 2, 3],
+        };
+        stack.push(4);
+        assert_eq!(vec![3, 2, 1, 4], stack.stack);
+        assert_eq!(vec![1, 2, 3, 4], stack.ordered);
+    }
 }
