92a6122579890fd290ff3f9fe880118d6cacb0db
CJ Harries <cj@wizardsoftheweb.pro>
2022-07-06 18:16:14 -0500

    Stub prompt fnc

--- easy/101-200/195/rust/src/main.rs
+++ easy/101-200/195/rust/src/main.rs
@@ -17,6 +17,10 @@ fn main() {
     println!("rad");
 }
 
+fn expand_symbolic_links(path: &str, links: Vec<(String, String)>) -> String {
+    String::new()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
