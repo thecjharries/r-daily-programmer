5149cad79f2e8767123ee3f338fe34240f60d2ac
CJ Harries <cj@wizardsoftheweb.pro>
2022-07-22 17:10:08 -0500

    Change VOWELS to CONSONANTS for foreign vowels

--- easy/201-300/212/rust/src/main.rs
+++ easy/201-300/212/rust/src/main.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const VOWELS: &str = "aeiou";
+const CONSONANTS: &str = "bcdfghjklmnpqrtsvwxz";
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
