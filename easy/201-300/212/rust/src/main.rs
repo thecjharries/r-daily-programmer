f74e13de77954a5a3db705b6a0180764dce9fb7f
CJ Harries <cj@wizardsoftheweb.pro>
2022-07-22 17:08:19 -0500

    Define vowels as singleton

--- easy/201-300/212/rust/src/main.rs
+++ easy/201-300/212/rust/src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const VOWELS: &str = "aeiou";
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
