47980f5792c983758c1266712474be7c4cf4b1f2
CJ Harries <cj@wizardsoftheweb.pro>
2022-07-24 08:41:02 -0500

    Add call to test fnc

--- easy/201-300/214/rust/src/main.rs
+++ easy/201-300/214/rust/src/main.rs
@@ -27,10 +27,15 @@ mod tests {
 
     #[test]
     fn test_calculate_standard_deviation() {
-        assert_eq!(9.7775, vec![5, 6, 11, 13, 19, 20, 25, 26, 28, 37]);
+        assert_eq!(
+            9.7775,
+            calculate_standard_deviation(vec![5, 6, 11, 13, 19, 20, 25, 26, 28, 37])
+        );
         assert_eq!(
             23.2908,
-            vec![37, 81, 86, 91, 97, 108, 109, 112, 112, 114, 115, 117, 121, 123, 141]
+            calculate_standard_deviation(vec![
+                37, 81, 86, 91, 97, 108, 109, 112, 112, 114, 115, 117, 121, 123, 141
+            ])
         );
     }
 }
