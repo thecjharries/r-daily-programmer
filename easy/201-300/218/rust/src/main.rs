209f33f57cd5aefcc0c900c21a8029e542d5e8b7
CJ Harries <cj@wizardsoftheweb.pro>
2022-07-28 17:04:16 -0500

    Create list type

--- easy/201-300/218/rust/src/main.rs
+++ easy/201-300/218/rust/src/main.rs
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+struct ToDoList {
+    todos: Vec<String>,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
