9a642dc905e56bc3769188560ef6db32cab92a7b
CJ Harries <cj@wizardsoftheweb.pro>
2022-07-29 17:37:26 -0500

    Test organize_sentence

--- easy/201-300/220/rust/src/main.rs
+++ easy/201-300/220/rust/src/main.rs
@@ -26,7 +26,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_organize_sentence() {
+        assert_eq!(
+            "Hist aceeghlln denos't eems os adhr.",
+            organize_sentence("This challenge doesn't seem so hard.")
+        );
+        assert_eq!("Eehrt aer emor ghinst beeentw aeehnv adn aehrt, Ahioort, ahnt aer ademrt fo in oruy hhilooppsy.", organize_sentence("There are more things between heaven and earth, Horatio, than are dreamt of in your philosophy."));
     }
 }
