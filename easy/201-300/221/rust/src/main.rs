2a288184a51a879d89e0d5ec0027f3e7f2b35658
CJ Harries <cj@wizardsoftheweb.pro>
2022-07-30 08:22:17 -0500

    Test build_word_snake

--- easy/201-300/221/rust/src/main.rs
+++ easy/201-300/221/rust/src/main.rs
@@ -27,6 +27,13 @@ mod tests {
 
     #[test]
     fn test_stub() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(
+            "SHENANIGANS
+          A
+          A
+          T
+          YOUNGSTER",
+            build_word_snake(vec!["SHENANIGANS", "SALTY", "YOUNGSTER"])
+        );
     }
 }
