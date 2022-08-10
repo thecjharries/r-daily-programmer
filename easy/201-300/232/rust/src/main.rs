2d1e0163efa23f645c21564f4f58ba482cb1dd4c
CJ Harries <cj@wizardsoftheweb.pro>
2022-08-10 09:08:28 -0500

    Add regex pattern

--- easy/201-300/232/rust/src/main.rs
+++ easy/201-300/232/rust/src/main.rs
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref NOT_LETTERS_PATTERN: Regex = Regex::new(r"[^a-z]").unwrap();
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
