2619ef28f1f9e39ad30f89341eb2f4e706f1d567
CJ Harries <cj@wizardsoftheweb.pro>
2022-09-17 09:40:09 -0500

    Implement transpose_text

--- easy/201-300/270/rust/src/main.rs
+++ easy/201-300/270/rust/src/main.rs
@@ -18,7 +18,27 @@ fn main() {
 }
 
 fn transpose_text(input: &str) -> String {
-    todo!()
+    let lines = input.split("\n").collect::<Vec<&str>>();
+    println!("{:?}", lines);
+    let mut output = Vec::new();
+    for (index, line) in lines.iter().enumerate() {
+        while output.len() < line.len() {
+            output.push(String::from(" ".repeat(index)));
+        }
+        println!("{:?}", output);
+        println!("{} {}", index, line);
+        for (character_index, character) in line.chars().enumerate() {
+            output[character_index].push(character);
+        }
+        for character_index in line.len()..output.len() {
+            output[character_index].push(' ');
+        }
+    }
+    output
+        .into_iter()
+        .map(|line| line.trim_end().to_string())
+        .collect::<Vec<String>>()
+        .join("\n")
 }
 
 #[cfg(test)]
@@ -29,7 +49,7 @@ mod tests {
     fn test_transpose_text() {
         assert_eq!(
             "St\noe\nmx\net\n .".to_string(),
-            transpose_text("Some text.")
+            transpose_text("Some\ntext.")
         );
         assert_eq!(
             "p i f       }\na m u\nc p n\nk o c\na r  qqqcf }\ng t muuulo\ne   aeeeor\n  \" iuuus\nm f neeeeef\na m (   (lm\ni t ):<<qet\nn \"  =--um.\n    {   e P\n     m\"\"u:r\n     aote=i\n     knw) n\n     eeo rt\n     (\"O al\n     c \" nn\n     h   g(\n     a   ee\n     n    l\n         qe\n     s   um\n     t   e)\n     r   u\n     i   e\n     n\n     g   {\n     ,\n\n     2\n     )" .to_string(),
