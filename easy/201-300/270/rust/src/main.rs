aeb8a620ab21221d15e4f1da9514cb8019646fcd
CJ Harries <cj@wizardsoftheweb.pro>
2022-09-17 09:28:03 -0500

    Test transpose_text

--- easy/201-300/270/rust/src/main.rs
+++ easy/201-300/270/rust/src/main.rs
@@ -26,7 +26,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_transpose_text() {
+        assert_eq!(
+            "St\noe\nmx\net\n .".to_string(),
+            transpose_text("Some text.")
+        );
+        assert_eq!(
+            "p i f       }\na m u\nc p n\nk o c\na r  qqqcf }\ng t muuulo\ne   aeeeor\n  \" iuuus\nm f neeeeef\na m (   (lm\ni t ):<<qet\nn \"  =--um.\n    {   e P\n     m\"\"u:r\n     aote=i\n     knw) n\n     eeo rt\n     (\"O al\n     c \" nn\n     h   g(\n     a   ee\n     n    l\n         qe\n     s   um\n     t   e)\n     r   u\n     i   e\n     n\n     g   {\n     ,\n\n     2\n     )" .to_string(),
+            transpose_text("package main\n\nimport \"fmt\"\n\nfunc main() {\n    queue := make(chan string, 2)\n    queue <- \"one\"\n    queue <- \"twoO\"\n    close(queue)\n    for elem := range queue {\n        fmt.Println(elem)\n    }\n}")
+        )
     }
 }
