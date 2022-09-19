69a4666c840b2620a11f69179e81dd6d7b8332db
CJ Harries <cj@wizardsoftheweb.pro>
2022-09-19 17:06:57 -0500

    Create struct for tileset

--- easy/201-300/272/rust/src/main.rs
+++ easy/201-300/272/rust/src/main.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
+
+struct ScrabbleTileSet {
+    tiles: HashMap<char, u8>,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
