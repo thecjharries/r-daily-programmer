9f8f22c47946d3241c5486933574ffb6798a9858
CJ Harries <cj@wizardsoftheweb.pro>
2022-10-01 11:31:21 -0500

    Implement find_swipe_words

--- easy/201-300/284/rust/src/main.rs
+++ easy/201-300/284/rust/src/main.rs
@@ -29,8 +29,24 @@ fn load_dictionary(path: &str) -> Vec<String> {
         .collect()
 }
 
-fn find_swipe_words(letters: &str, min_length: usize, dictionary: Vec<String>) -> Vec<String> {
-    todo!()
+fn find_swipe_words(letters: &str, min_length: usize, dictionary: &Vec<String>) -> Vec<String> {
+    dictionary
+        .iter()
+        .filter(|word| &word[0..1] == &letters[0..1])
+        .filter(|word| word.len() >= min_length)
+        .filter(|word| {
+            let mut current_index = 0;
+            for letter in word.chars() {
+                if let Some(index) = letters[current_index..].find(letter) {
+                    current_index = index;
+                } else {
+                    return false;
+                }
+            }
+            true
+        })
+        .map(|word| word.to_string())
+        .collect()
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -49,33 +65,33 @@ mod tests {
 
     #[test]
     fn test_find_swipe_words() {
-        let dictionary = vec![
-            "answer",
-            "bee",
-            "cab",
-            "gaeing",
-            "gathering",
-            "gating",
-            "ghost",
-            "giant",
-            "gieing",
-            "gig",
-            "gigantic",
-            "giggle",
-            "going",
-            "goring",
-            "one",
-            "question",
-            "three",
-            "two",
+        let dictionary: Vec<String> = vec![
+            "answer".to_string(),
+            "bee".to_string(),
+            "cab".to_string(),
+            "gaeing".to_string(),
+            "gathering".to_string(),
+            "gating".to_string(),
+            "ghost".to_string(),
+            "giant".to_string(),
+            "gieing".to_string(),
+            "gig".to_string(),
+            "gigantic".to_string(),
+            "giggle".to_string(),
+            "going".to_string(),
+            "goring".to_string(),
+            "one".to_string(),
+            "question".to_string(),
+            "three".to_string(),
+            "two".to_string(),
         ];
         assert_eq!(
             vec!["question"],
-            find_swipe_words("qwertyuytresdftyuioknn", 5, dictionary)
+            find_swipe_words("qwertyuytresdftyuioknn", 5, &dictionary)
         );
         assert_eq!(
             vec!["gaeing", "gathering", "gating", "gieing", "going", "goring"],
-            find_swipe_words("gijakjthoijerjidsdfnokg", 5, dictionary)
+            find_swipe_words("gijakjthoijerjidsdfnokg", 5, &dictionary)
         );
     }
 }
