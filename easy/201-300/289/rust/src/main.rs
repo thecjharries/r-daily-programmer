1cbd1fdddc28ec7d1310254dc0f66a5ee8508925
CJ Harries <cj@wizardsoftheweb.pro>
2022-10-06 17:53:13 -0500

    Test determine_damage_multipler

--- easy/201-300/289/rust/src/main.rs
+++ easy/201-300/289/rust/src/main.rs
@@ -12,13 +12,43 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct PokemonType {
+    name: String,
+    url: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct PokemonTypeDamageRelations {
+    double_damage_from: Vec<PokemonType>,
+    double_damage_to: Vec<PokemonType>,
+    half_damage_from: Vec<PokemonType>,
+    half_damage_to: Vec<PokemonType>,
+    no_damage_from: Vec<PokemonType>,
+    no_damage_to: Vec<PokemonType >,
+}
+
+#[derive(Deserialize, Debug)]
+struct PokemonTypeQuery {
+    damage_relations: PokemonTypeDamageRelations,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
 }
 
-fn determine_damage_multipler(attacker_type: &str, defender_type: &str) -> u8 {
-    todo!()
+#[tokio::main]
+async fn determine_damage_multipler(attack_type: &str, defender_types: Vec<&str>) -> Result<f32, Error> {
+    let request_url = format!("https://pokeapi.co/api/v2/type/{}", "fire");
+    println!("{}", request_url);
+    let response = reqwest::get(&request_url).await?;
+    let users: PokemonTypeQuery = response.json().await?;
+    println!("{:?}", users);
+    Ok(0.0)
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -26,8 +56,12 @@ fn determine_damage_multipler(attacker_type: &str, defender_type: &str) -> u8 {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    #[tokio::test]
+    async fn test_stub() {
+        assert_eq!(2.0, determine_damage_multipler("fire", vec!["grass"]).await.unwrap());
+        assert_eq!(4.0, determine_damage_multipler("fighting", vec!["ice", "rock"]).await.unwrap());
+        assert_eq!(0.0, determine_damage_multipler("psychic", vec!["poison", "dark"]).await.unwrap());
+        assert_eq!(1.0, determine_damage_multipler("water", vec!["normal"]).await.unwrap());
+        assert_eq!(0.5, determine_damage_multipler("fire", vec!["rock"]).await.unwrap());
     }
 }
