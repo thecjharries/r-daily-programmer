0babf089b33efd7455148a45c7db50ee36a5004c
CJ Harries <cj@wizardsoftheweb.pro>
2022-10-07 17:16:06 -0500

    Implement play_mp3

--- easy/201-300/300/rust/src/main.rs
+++ easy/201-300/300/rust/src/main.rs
@@ -12,15 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use rodio::{source::Source, Decoder, OutputStream};
 use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
 }
 
-fn play_mp3(path: &str) -> Result<(), dyn Error> {
-    todo!()
+fn play_mp3(path: &str) -> Result<(), Box<dyn Error>> {
+    // Body pulled from https://docs.rs/rodio/latest/rodio/
+
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open(path).unwrap());
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+    // Play the sound directly on the device
+    stream_handle.play_raw(source.convert_samples());
+
+    // The sound plays in a separate audio thread,
+    // so we need to keep the main thread alive while it's playing.
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    Ok(())
 }
 
 #[cfg(not(tarpaulin_include))]
