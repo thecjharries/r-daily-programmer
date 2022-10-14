2b1ee279e5f25a1893b1fa189e570bea1a9a89aa
CJ Harries <cj@wizardsoftheweb.pro>
2022-10-14 17:39:15 -0500

    Test build_presents_song

--- easy/201-300/296/rust/src/main.rs
+++ easy/201-300/296/rust/src/main.rs
@@ -28,6 +28,22 @@ mod tests {
 
     #[test]
     fn test_stub() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(
+            "On the first day of Christmas\nmy true love sent to me:\n1 Partridge in a Pear Tree\n\nOn the second day of Christmas\nmy true love sent to me:\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the third day of Christmas\nmy true love sent to me:\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the fourth day of Christmas\nmy true love sent to me:\n4 Calling Birds\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the fifth day of Christmas\nmy true love sent to me:\n5 Golden Rings\n4 Calling Birds\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the sixth day of Christmas\nmy true love sent to me:\n6 Geese a Laying\n5 Golden Rings\n4 Calling Birds\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the seventh day of Christmas\nmy true love sent to me:\n7 Swans a Swimming\n6 Geese a Laying\n5 Golden Rings\n4 Calling Birds\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the eighth day of Christmas\nmy true love sent to me:\n8 Maids a Milking\n7 Swans a Swimming\n6 Geese a Laying\n5 Golden Rings\n4 Calling Birds\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the ninth day of Christmas\nmy true love sent to me:\n9 Ladies Dancing\n8 Maids a Milking\n7 Swans a Swimming\n6 Geese a Laying\n5 Golden Rings\n4 Calling Birds\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the tenth day of Christmas\nmy true love sent to me:\n10 Lords a Leaping\n9 Ladies Dancing\n8 Maids a Milking\n7 Swans a Swimming\n6 Geese a Laying\n5 Golden Rings\n4 Calling Birds\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the eleventh day of Christmas\nmy true love sent to me:\n11 Pipers Piping\n10 Lords a Leaping\n9 Ladies Dancing\n8 Maids a Milking\n7 Swans a Swimming\n6 Geese a Laying\n5 Golden Rings\n4 Calling Birds\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree\n\nOn the twelfth day of Christmas\nmy true love sent to me:\n12 Drummers Drumming\n11 Pipers Piping\n10 Lords a Leaping\n9 Ladies Dancing\n8 Maids a Milking\n7 Swans a Swimming\n6 Geese a Laying\n5 Golden Rings\n4 Calling Birds\n3 French Hens\n2 Turtle Doves\nand 1 Partridge in a Pear Tree".to_string(),
+            build_presents_song(vec![
+                "Partridge in a Pear Tree".to_string(),
+                "Turtle Doves".to_string(),
+                "French Hens".to_string(),
+                "Calling Birds".to_string(),
+                "Golden Rings".to_string(),
+                "Geese a Laying".to_string(),
+                "Swans a Swimming".to_string(),
+                "Maids a Milking".to_string(),
+                "Ladies Dancing".to_string(),
+                "Lords a Leaping".to_string(),
+                "Pipers Piping".to_string(),
+                "Drummers Drumming".to_string(),
+            ])
+        );
     }
 }
