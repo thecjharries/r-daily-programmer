f229be6365316b96f35e33eba71a1d675764a91c
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-05 11:22:03 -0500

    Implement find_zero_sum_triplets

--- easy/301-400/323/rust/src/main.rs
+++ easy/301-400/323/rust/src/main.rs
@@ -18,7 +18,28 @@ fn main() {
 }
 
 fn find_zero_sum_triplets(input: Vec<i32>) -> Vec<Vec<i32>> {
-    todo!();
+    let mut result = Vec::new();
+    for (index, item) in input.iter().enumerate() {
+        for (index2, item2) in input.iter().enumerate() {
+            for (index3, item3) in input.iter().enumerate() {
+                if index != index2
+                    && index != index3
+                    && index2 != index3
+                    && item + item2 + item3 == 0
+                {
+                    let mut triplet = vec![*item, *item2, *item3];
+                    triplet.sort();
+                    if vec![-8, 4, 4] == triplet {
+                        println!("{} {} {}", index, index2, index3);
+                    }
+                    if !result.contains(&triplet) {
+                        result.push(triplet);
+                    }
+                }
+            }
+        }
+    }
+    result
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -31,13 +52,11 @@ mod tests {
         assert_eq!(
             vec![
                 vec![-5, -4, 9],
-                vec![-6, 3, 3],
                 vec![-5, 1, 4],
                 vec![-4, -4, 8],
                 vec![-9, 1, 8],
                 vec![-4, 1, 3],
                 vec![-8, 1, 7],
-                vec![-8, 4, 4]
             ],
             find_zero_sum_triplets(vec![
                 9, -6, -5, 9, 8, 3, -4, 8, 1, 7, -4, 9, -9, 1, 9, -9, 9, 4, -6, -8
@@ -46,26 +65,19 @@ mod tests {
         assert_eq!(
             vec![
                 vec![-3, -1, 4],
-                vec![-2, -2, 4],
                 vec![-5, 1, 4],
                 vec![-3, -2, 5],
                 vec![-7, 2, 5],
-                vec![-1, -1, 2],
                 vec![-3, 1, 2]
             ],
             find_zero_sum_triplets(vec![4, 5, -1, -2, -7, 2, -5, -3, -7, -3, 1])
         );
         assert_eq!(
-            vec![
-                vec![-1, -1, 2],
-                vec![-6, 1, 5],
-                vec![-3, 1, 2],
-                vec![-7, 2, 5]
-            ],
+            vec![vec![-6, 1, 5], vec![-3, 1, 2], vec![-7, 2, 5]],
             find_zero_sum_triplets(vec![-1, -6, -3, -7, 5, -8, 2, -8, 1])
         );
         assert_eq!(
-            vec![vec![-5, -4, 9], vec![-1, -1, 2], vec![-4, 2, 2]],
+            vec![vec![-5, -4, 9], vec![-1, -1, 2]],
             find_zero_sum_triplets(vec![-5, -1, -4, 2, 9, -9, -6, -1, -7])
         );
     }
