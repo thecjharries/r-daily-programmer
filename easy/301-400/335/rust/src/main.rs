1acfd01b34571f0fc8dcfa4309103d02ae600665
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-12 08:35:41 -0600

    Test calculate_consecutive_distance_rating

--- easy/301-400/335/rust/src/main.rs
+++ easy/301-400/335/rust/src/main.rs
@@ -28,6 +28,31 @@ mod tests {
 
     #[test]
     fn test_stub() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(
+            26,
+            calculate_consecutive_distance_rating(vec![31, 63, 53, 56, 96, 62, 73, 25, 54, 55, 64])
+        );
+        assert_eq!(
+            20,
+            calculate_consecutive_distance_rating(vec![77, 39, 35, 38, 41, 42, 76, 73, 40, 31, 10])
+        );
+        assert_eq!(
+            15,
+            calculate_consecutive_distance_rating(vec![30, 63, 57, 87, 37, 31, 58, 83, 34, 76, 38])
+        );
+        assert_eq!(
+            3,
+            calculate_consecutive_distance_rating(vec![18, 62, 55, 92, 88, 57, 90, 10, 11, 96, 12])
+        );
+        assert_eq!(
+            6,
+            calculate_consecutive_distance_rating(vec![26, 8, 7, 25, 52, 17, 45, 64, 11, 35, 12])
+        );
+        assert_eq!(
+            13,
+            calculate_consecutive_distance_rating(vec![
+                89, 57, 21, 55, 56, 81, 54, 100, 22, 62, 50
+            ])
+        );
     }
 }
