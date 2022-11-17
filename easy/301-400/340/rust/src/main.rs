cbba7ba8fec3526893e2383f6aba826d43315cce
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-17 17:14:42 -0600

    Implement find_first_repeated_character

--- easy/301-400/340/rust/src/main.rs
+++ easy/301-400/340/rust/src/main.rs
@@ -12,13 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
 }
 
 fn find_first_repeated_character(input: &str) -> Result<(char, usize), String> {
-    todo!()
+    let mut character_counts: HashMap<char, usize> = HashMap::new();
+    for (index, character) in input.chars().enumerate() {
+        if character_counts.contains_key(&character) {
+            return Ok((character, index));
+        }
+        character_counts.insert(character, index);
+    }
+    Err(String::from("No repeated characters"))
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -29,6 +38,9 @@ mod tests {
     #[test]
     fn test_find_first_repeated_character() {
         assert_eq!(Ok(('B', 4)), find_first_repeated_character("ABCDBE"));
-        assert_eq!(Err(String::from("")), find_first_repeated_character("ABCD"));
+        assert_eq!(
+            Err(String::from("No repeated characters")),
+            find_first_repeated_character("ABCD")
+        );
     }
 }
