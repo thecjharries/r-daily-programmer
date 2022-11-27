bf9165096e20e1e27b200c53a37d17fa3638be5e
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-27 11:03:46 -0600

    Take quick stab at solving

--- easy/301-400/351/rust/src/main.rs
+++ easy/301-400/351/rust/src/main.rs
@@ -20,7 +20,30 @@ fn main() {
 }
 
 fn score_cricket_inning(score: &str) -> HashMap<String, u32> {
-    todo!()
+    let mut score_map = HashMap::new();
+    let mut current_player = 1;
+    for score in score.chars() {
+        if '.' == score {
+            continue;
+        }
+        if 'W' == score {
+            current_player = (current_player + 1) % 3 + 1;
+            continue;
+        }
+        if 'b' == score || 'w' == score {
+            score_map
+                .entry("Extras".to_string())
+                .and_modify(|x| *x += 1)
+                .or_insert(1);
+            continue;
+        }
+        let player = format!("P{}", current_player);
+        score_map
+            .entry(player)
+            .and_modify(|x| *x += score.to_digit(10).unwrap())
+            .or_insert(score.to_digit(10).unwrap());
+    }
+    score_map
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -31,15 +54,12 @@ mod tests {
     #[test]
     fn test_score_cricket_inning() {
         assert_eq!(
-            [
+            HashMap::from_iter([
                 ("P1".to_string(), 7),
                 ("P2".to_string(), 2),
                 ("P3".to_string(), 9),
                 ("Extras".to_string(), 2)
-            ]
-            .iter()
-            .cloned()
-            .collect(),
+            ]),
             score_cricket_inning("1.2wW6.2b34")
         )
     }
