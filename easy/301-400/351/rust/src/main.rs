ba9e081eb07e8c7a1fba6a5215e96770242d802a
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-27 10:55:54 -0600

    Stub prompt fnc

--- easy/301-400/351/rust/src/main.rs
+++ easy/301-400/351/rust/src/main.rs
@@ -12,9 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
+}
+
+fn score_cricket_inning(score: &str) -> HashMap<String, u32> {
+    todo!()
 }
 
 #[cfg(not(tarpaulin_include))]
