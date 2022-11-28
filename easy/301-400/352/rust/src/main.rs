fc02edd6f713c3ad1e9ef32d6474c71277d34890
CJ Harries <cj@wizardsoftheweb.pro>
2022-11-28 17:07:19 -0600

    Define base 62 alphabet

--- easy/301-400/352/rust/src/main.rs
+++ easy/301-400/352/rust/src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const BASE_62_ALPHABET: &str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
