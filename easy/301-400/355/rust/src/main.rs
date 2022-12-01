c6dac003eb19bc42ebeb98bbdc342151829fbc70
CJ Harries <cj@wizardsoftheweb.pro>
2022-12-01 17:42:22 -0600

    Stub prompt fnc

--- easy/301-400/355/rust/src/main.rs
+++ easy/301-400/355/rust/src/main.rs
@@ -17,6 +17,10 @@ fn main() {
     println!("rad");
 }
 
+fn encrypt_alphabet_cipher(keyword: &str, plaintext: &str) -> String {
+    todo!()
+}
+
 #[cfg(not(tarpaulin_include))]
 #[cfg(test)]
 mod tests {
