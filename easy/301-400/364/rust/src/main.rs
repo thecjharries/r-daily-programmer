68582cc87ec138b5794895b963a3e04f59ab451a
CJ Harries <cj@wizardsoftheweb.pro>
2022-12-06 10:15:50 -0600

    Add regex for dice pattern

--- easy/301-400/364/rust/src/main.rs
+++ easy/301-400/364/rust/src/main.rs
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref DICE_PATTERN: Regex = Regex::new(r"(?P<count>\d+)d(?P<sides>\d+)").unwrap();
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
