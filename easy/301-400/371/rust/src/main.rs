26efbfdfdb57132c3263703be67b7c283eb13b6d
CJ Harries <cj@wizardsoftheweb.pro>
2022-12-12 13:48:43 -0600

    Implement qcheck

--- easy/301-400/371/rust/src/main.rs
+++ easy/301-400/371/rust/src/main.rs
@@ -18,7 +18,24 @@ fn main() {
 }
 
 fn qcheck(places: Vec<u8>) -> bool {
-    todo!()
+    for (first_index, first_place) in places.iter().enumerate() {
+        for (second_index, second_place) in places.iter().enumerate() {
+            if first_index == second_index {
+                continue;
+            }
+            if first_place == second_place {
+                return false;
+            }
+            if 1.0
+                == ((*first_place as f64 - *second_place as f64)
+                    / (first_index as f64 - second_index as f64))
+                    .abs()
+            {
+                return false;
+            }
+        }
+    }
+    true
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -28,10 +45,10 @@ mod tests {
 
     #[test]
     fn test_stub() {
-        assert_eq!(true, qcheck(vec![4, 2, 7, 3, 6, 8, 5, 1]))
-        assert_eq!(true, qcheck(vec![2, 5, 7, 4, 1, 8, 6, 3]))
-        assert_eq!(false, qcheck(vec![5, 3, 1, 4, 2, 8, 6, 3]))
-        assert_eq!(false, qcheck(vec![5, 8, 2, 4, 7, 1, 3, 6]))
-        assert_eq!(false, qcheck(vec![4, 3, 1, 8, 1, 3, 5, 2]))
+        assert_eq!(true, qcheck(vec![4, 2, 7, 3, 6, 8, 5, 1]));
+        assert_eq!(true, qcheck(vec![2, 5, 7, 4, 1, 8, 6, 3]));
+        assert_eq!(false, qcheck(vec![5, 3, 1, 4, 2, 8, 6, 3]));
+        assert_eq!(false, qcheck(vec![5, 8, 2, 4, 7, 1, 3, 6]));
+        assert_eq!(false, qcheck(vec![4, 3, 1, 8, 1, 3, 5, 2]));
     }
 }
