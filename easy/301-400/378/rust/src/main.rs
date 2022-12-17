eebadc69f3768ff17e74026de0813545e44ca4ed
CJ Harries <cj@wizardsoftheweb.pro>
2022-12-17 15:33:31 -0600

    Test havel_hakimi

--- easy/301-400/378/rust/src/main.rs
+++ easy/301-400/378/rust/src/main.rs
@@ -27,7 +27,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_havel_hakimi() {
+        assert_eq!(havel_hakimi(vec![5, 3, 0, 2, 6, 2, 0, 7, 2, 5]), false);
+        assert_eq!(havel_hakimi(vec![4, 2, 0, 1, 5, 0]), false);
+        assert_eq!(havel_hakimi(vec![3, 1, 2, 3, 1, 0]), true);
+        assert_eq!(
+            havel_hakimi(vec![
+                16, 9, 9, 15, 9, 7, 9, 11, 17, 11, 4, 9, 12, 14, 14, 12, 17, 0, 3, 16
+            ]),
+            true
+        );
+        assert_eq!(
+            havel_hakimi(vec![
+                14, 10, 17, 13, 4, 8, 6, 7, 13, 13, 17, 18, 8, 17, 2, 14, 6, 4, 7, 12
+            ]),
+            true
+        );
+        assert_eq!(
+            havel_hakimi(vec![
+                15, 18, 6, 13, 12, 4, 4, 14, 1, 6, 18, 2, 6, 16, 0, 9, 10, 7, 12, 3
+            ]),
+            false
+        );
+        assert_eq!(
+            havel_hakimi(vec![
+                6, 0, 10, 10, 10, 5, 8, 3, 0, 14, 16, 2, 13, 1, 2, 13, 6, 15, 5, 1
+            ]),
+            false
+        );
+        assert_eq!(havel_hakimi(vec![2, 2, 0]), false);
+        assert_eq!(havel_hakimi(vec![3, 2, 1]), false);
+        assert_eq!(havel_hakimi(vec![1, 1]), true);
+        assert_eq!(havel_hakimi(vec![1]), false);
+        assert_eq!(havel_hakimi(vec![]), true);
     }
 }
