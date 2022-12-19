20114d4c01dd99e4c322d1aac3d6f35e89ebcf99
CJ Harries <cj@wizardsoftheweb.pro>
2022-12-19 17:07:46 -0600

    Test smorse

--- easy/301-400/380/rust/src/main.rs
+++ easy/301-400/380/rust/src/main.rs
@@ -82,6 +82,13 @@ mod tests {
 
     #[test]
     fn test_stub() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!("...---...".to_string(), smorse("sos"));
+        assert_eq!("-...-...-..-.--".to_string(), smorse("daily"));
+        assert_eq!(
+            ".--..-.-----..-..-----..-.".to_string(),
+            smorse("programmer")
+        );
+        assert_eq!("-.....-...".to_string(), smorse("bits"));
+        assert_eq!("-.....-...".to_string(), smorse("three"));
     }
 }
