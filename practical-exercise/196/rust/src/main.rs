0f6625e785dc309aadc197ce8cb579939a3c0e68
CJ Harries <cj@wizardsoftheweb.pro>
2022-12-25 11:21:13 -0600

    Create GenericSet struct

--- practical-exercise/196/rust/src/main.rs
+++ practical-exercise/196/rust/src/main.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::BTreeMap;
+
+struct GenericSet<T> {
+    items: BTreeMap<T, bool>,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
