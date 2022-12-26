678469eaa79e75c7a0a9972967030fe9c58718f6
CJ Harries <cj@wizardsoftheweb.pro>
2022-12-26 13:03:26 -0600

    Create coin slice

--- easy/301-400/393/rust/src/main.rs
+++ easy/301-400/393/rust/src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+static COINS: [u32; 6] = [1, 5, 10, 25, 100, 500];
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
