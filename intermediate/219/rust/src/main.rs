87e6bd5b47ab7405d2734ffa8439057188f6ae01
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-02 12:13:40 -0600

    Define TodoList

--- intermediate/219/rust/src/main.rs
+++ intermediate/219/rust/src/main.rs
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+struct TodoList {
+    items: Vec<String>,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
