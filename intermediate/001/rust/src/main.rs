0196dc64252e1151b4809d056f2798185fcd947e
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-03 17:25:51 -0600

    Create and test EventCalendar::add_event

--- intermediate/001/rust/src/main.rs
+++ intermediate/001/rust/src/main.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use chrono::NaiveDateTime;
+use chrono::{Datelike, NaiveDate, NaiveDateTime, Timelike};
 use std::collections::HashMap;
 
 struct EventCalendar(HashMap<NaiveDateTime, Vec<(NaiveDateTime, String)>>);
@@ -20,6 +20,18 @@ struct EventCalendar(HashMap<NaiveDateTime, Vec<(NaiveDateTime, String)>>);
 impl EventCalendar {
     fn new() -> Self {
         Self(HashMap::new())
+    }
+
+    fn add_event(&mut self, start: NaiveDateTime, event: String) {
+        let at_hour: NaiveDateTime =
+            NaiveDate::from_ymd_opt(start.year(), start.month(), start.day())
+                .unwrap()
+                .and_hms_opt(start.hour(), 0, 0)
+                .unwrap();
+        self.0
+            .entry(at_hour)
+            .or_insert(Vec::new())
+            .push((start, event));
     }
 }
 
@@ -37,5 +49,20 @@ mod tests {
     fn test_new() {
         let calendar = EventCalendar::new();
         assert_eq!(0, calendar.0.len());
+    }
+
+    #[test]
+    fn test_add_event() {
+        let mut calendar = EventCalendar::new();
+        let start: NaiveDateTime = NaiveDate::from_ymd_opt(2022, 1, 1)
+            .unwrap()
+            .and_hms_opt(0, 0, 0)
+            .unwrap();
+        let event = "Test Event".to_string();
+        calendar.add_event(start, event.clone());
+        assert_eq!(1, calendar.0.len());
+        assert_eq!(1, calendar.0[&start].len());
+        assert_eq!(start, calendar.0[&start][0].0);
+        assert_eq!(event, calendar.0[&start][0].1);
     }
 }
