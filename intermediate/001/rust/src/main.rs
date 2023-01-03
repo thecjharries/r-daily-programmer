e65c58b1ce43046574196f7f5c232d5e239128b4
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-03 17:15:15 -0600

    Create EventCalendar

--- intermediate/001/rust/src/main.rs
+++ intermediate/001/rust/src/main.rs
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
+
+struct EventCalendar(HashMap<String, Vec<String>>);
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
