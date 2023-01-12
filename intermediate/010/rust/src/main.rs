99a1642dfcd0f8123c10580b7570dd5bb780c6d9
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-12 11:01:34 -0600

    Implement is_equation_palindrome

--- intermediate/010/rust/src/main.rs
+++ intermediate/010/rust/src/main.rs
@@ -12,13 +12,55 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use num_to_words::integer_to_en_us;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
 }
 
 fn is_equation_palindrome(equation: &str) -> bool {
-    todo!()
+    let equation = equation.replace(" ", "");
+    let equation_parts = equation.split('=').collect::<Vec<&str>>();
+    if equation_parts.len() != 2 {
+        return false;
+    }
+    let left_side = equation_parts[0];
+    let right_side = equation_parts[1];
+    let left_side_parts = left_side.split('+').collect::<Vec<&str>>();
+    let right_side_parts = right_side.split('+').collect::<Vec<&str>>();
+    let left_side_sum: u32 = left_side_parts
+        .iter()
+        .map(|part| part.parse::<u32>().unwrap())
+        .sum();
+    let right_side_sum: u32 = right_side_parts
+        .iter()
+        .map(|part| part.parse::<u32>().unwrap())
+        .sum();
+    if left_side_sum != right_side_sum {
+        return false;
+    }
+    let mut left_side_chars: Vec<char> = Vec::new();
+    for number in left_side_parts {
+        for word in integer_to_en_us(number.parse::<i64>().unwrap())
+            .unwrap()
+            .split(' ')
+        {
+            left_side_chars.extend(word.chars());
+        }
+    }
+    left_side_chars.sort();
+    let mut right_side_chars: Vec<char> = Vec::new();
+    for number in right_side_parts {
+        for word in integer_to_en_us(number.parse::<i64>().unwrap())
+            .unwrap()
+            .split(' ')
+        {
+            right_side_chars.extend(word.chars());
+        }
+    }
+    right_side_chars.sort();
+    left_side_chars == right_side_chars
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -31,5 +73,6 @@ mod tests {
         assert_eq!(false, is_equation_palindrome("stub"));
         assert_eq!(true, is_equation_palindrome("1+12=11+2"));
         assert_eq!(true, is_equation_palindrome("1+22=2+21"));
+        assert_eq!(false, is_equation_palindrome("1+22=2+22"));
     }
 }
