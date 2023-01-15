56fda9d9702a44882d63da9d678f61e37cf3b92c
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-15 11:03:17 -0600

    Test reverse_string

--- intermediate/013/rust/src/main.rs
+++ intermediate/013/rust/src/main.rs
@@ -28,6 +28,7 @@ mod tests {
 
     #[test]
     fn test_stub() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!("!olleh", reverse_string("hello!"));
+        assert_eq!("!dlrow ,olleh", reverse_string("hello world!"));
     }
 }
