e61913030273adccfd49cc2fe7704b6bef03d47e
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-17 09:08:19 -0600

    Define constants

--- intermediate/015/rust/src/main.rs
+++ intermediate/015/rust/src/main.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const MIN: usize = 0;
+const MAX: usize = 29;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
