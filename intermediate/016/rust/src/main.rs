f94fb90ff57b83b8bfef68b3c317ead88fff5498
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-18 09:29:52 -0600

    Create and test CrapsGame::roll

--- intermediate/016/rust/src/main.rs
+++ intermediate/016/rust/src/main.rs
@@ -39,6 +39,28 @@ impl CrapsGame<'_> {
             status: CrapsGameStatus::Point(0),
         }
     }
+
+    fn roll(&mut self) -> u8 {
+        let roll = self.rng.gen_range(1..7) + self.rng.gen_range(1..7);
+        if 0 == self.point {
+            if 7 == roll || 11 == roll {
+                self.status = CrapsGameStatus::Win;
+            } else if 2 == roll || 3 == roll || 12 == roll {
+                self.status = CrapsGameStatus::Loss;
+            } else {
+                self.point = roll;
+                self.status = CrapsGameStatus::Point(roll);
+            }
+        } else {
+            if self.point == roll {
+                self.status = CrapsGameStatus::Win;
+            } else if 7 == roll {
+                self.status = CrapsGameStatus::Loss;
+            }
+        }
+        self.rolls.push(roll);
+        roll
+    }
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -52,11 +74,39 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
+    fn test_crapsgame_new() {
         let mut rng = Pcg64::seed_from_u64(0);
         let game = CrapsGame::new(&mut rng);
         assert_eq!(0, game.point);
         assert_eq!(CrapsGameStatus::Point(0), game.status);
         assert_eq!(0, game.rolls.len());
+    }
+
+    #[test]
+    fn test_crapsgame_roll() {
+        let mut rng0 = Pcg64::seed_from_u64(0);
+        let mut game0 = CrapsGame::new(&mut rng0);
+        game0.roll();
+        assert_eq!(CrapsGameStatus::Win, game0.status);
+        assert_eq!(1, game0.rolls.len());
+        let mut rng1 = Pcg64::seed_from_u64(1);
+        let mut game1 = CrapsGame::new(&mut rng1);
+        game1.roll();
+        assert_eq!(CrapsGameStatus::Point(5), game1.status);
+        assert_eq!(1, game1.rolls.len());
+        game1.roll();
+        assert_eq!(CrapsGameStatus::Loss, game1.status);
+        assert_eq!(2, game1.rolls.len());
+        let mut rng2 = Pcg64::seed_from_u64(50);
+        let mut game2 = CrapsGame::new(&mut rng2);
+        game2.roll();
+        assert_eq!(CrapsGameStatus::Loss, game2.status);
+        assert_eq!(1, game2.rolls.len());
+        let mut rng3 = Pcg64::seed_from_u64(800);
+        let mut game3 = CrapsGame::new(&mut rng3);
+        game3.roll();
+        game3.roll();
+        assert_eq!(CrapsGameStatus::Win, game3.status);
+        assert_eq!(2, game3.rolls.len());
     }
 }
