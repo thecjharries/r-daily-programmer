247d5f05d8d4a2d03fdd3137cb30f40533a101c4
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-18 09:20:00 -0600

    Define and test CrapsGame::new

--- intermediate/016/rust/src/main.rs
+++ intermediate/016/rust/src/main.rs
@@ -12,19 +12,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use rng::Rng;
+use rand::prelude::*;
+use rand_pcg::Pcg64;
 
+#[derive(Debug, PartialEq)]
 enum CrapsGameStatus {
     Point(u8),
     Win,
     Loss,
 }
 
-struct CrapsGame {
-    rng: Rng,
+#[derive(Debug)]
+struct CrapsGame<'a> {
+    rng: &'a mut Pcg64,
     rolls: Vec<u8>,
     point: u8,
     status: CrapsGameStatus,
+}
+
+impl CrapsGame<'_> {
+    fn new(rng: &mut Pcg64) -> CrapsGame {
+        CrapsGame {
+            rng,
+            rolls: Vec::new(),
+            point: 0,
+            status: CrapsGameStatus::Point(0),
+        }
+    }
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -38,7 +52,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_new() {
+        let mut rng = Pcg64::seed_from_u64(0);
+        let game = CrapsGame::new(&mut rng);
+        assert_eq!(0, game.point);
+        assert_eq!(CrapsGameStatus::Point(0), game.status);
+        assert_eq!(0, game.rolls.len());
     }
 }
