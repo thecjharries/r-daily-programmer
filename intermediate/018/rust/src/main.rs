6a38016d9eeda23fc1ea61eed358ffdaab26703a
CJ Harries <cj@wizardsoftheweb.pro>
2023-02-01 08:55:26 -0600

    Create prompt data structure

--- intermediate/018/rust/src/main.rs
+++ intermediate/018/rust/src/main.rs
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::HashMap;
+
+struct PromptCollection(HashMap<char, String>);
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
