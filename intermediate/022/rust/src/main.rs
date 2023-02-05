cbf45bde55d2058e8f3942a12a76fdf01327a4a6
CJ Harries <cj@wizardsoftheweb.pro>
2023-02-05 08:09:50 -0600

    Create and test Grid::from_str

--- intermediate/022/rust/src/main.rs
+++ intermediate/022/rust/src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::str::FromStr;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
@@ -20,13 +22,32 @@ fn main() {
 #[derive(Debug, PartialEq)]
 struct Grid(Vec<Vec<char>>);
 
+impl FromStr for Grid {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut grid = Grid(Vec::new());
+        for line in input.lines() {
+            grid.0.push(line.chars().collect());
+        }
+        Ok(grid)
+    }
+}
+
 #[cfg(not(tarpaulin_include))]
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_grid_from_str() {
+        let input = "###\n#.#\n###";
+        let expected = Grid(vec![
+            vec!['#', '#', '#'],
+            vec!['#', '.', '#'],
+            vec!['#', '#', '#'],
+        ]);
+        assert_eq!(expected, input.parse::<Grid>().unwrap());
+        assert_eq!(expected, Grid::from_str(input).unwrap());
     }
 }
