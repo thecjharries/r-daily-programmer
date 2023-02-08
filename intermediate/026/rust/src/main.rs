75fb7b650a887b781416d3c045859312200c91e8
CJ Harries <cj@wizardsoftheweb.pro>
2023-02-08 09:06:47 -0600

    Create Employee Struct

--- intermediate/026/rust/src/main.rs
+++ intermediate/026/rust/src/main.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+struct Employee {
+    name String,
+    age u32,
+    salary f32,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
