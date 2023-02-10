d98a80b1abf34f2b282252bc545ea08794aa3034
CJ Harries <cj@wizardsoftheweb.pro>
2023-02-09 20:35:43 -0600

    Rename fn for clarity

--- intermediate/028/rust/src/main.rs
+++ intermediate/028/rust/src/main.rs
@@ -19,7 +19,7 @@ fn main() {
     println!("rad");
 }
 
-fn find_tetrahedral_number(input: u64) -> u64 {
+fn find_tetrahedral_number_base(input: u64) -> u64 {
     if 0 == input {
         return 0;
     }
@@ -41,16 +41,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_find_tetrahedral_number() {
-        assert_eq!(0, find_tetrahedral_number(0));
-        assert_eq!(1, find_tetrahedral_number(1));
-        assert_eq!(3, find_tetrahedral_number(2));
-        assert_eq!(3, find_tetrahedral_number(3));
-        assert_eq!(3, find_tetrahedral_number(4));
-        assert_eq!(6, find_tetrahedral_number(5));
-        assert_eq!(6, find_tetrahedral_number(6));
-        assert_eq!(6, find_tetrahedral_number(7));
-        assert_eq!(6, find_tetrahedral_number(8));
-        assert_eq!(505013002528, find_tetrahedral_number(169179692512835000));
+    fn test_find_tetrahedral_number_base() {
+        assert_eq!(0, find_tetrahedral_number_base(0));
+        assert_eq!(1, find_tetrahedral_number_base(1));
+        assert_eq!(3, find_tetrahedral_number_base(2));
+        assert_eq!(3, find_tetrahedral_number_base(3));
+        assert_eq!(3, find_tetrahedral_number_base(4));
+        assert_eq!(6, find_tetrahedral_number_base(5));
+        assert_eq!(6, find_tetrahedral_number_base(6));
+        assert_eq!(6, find_tetrahedral_number_base(7));
+        assert_eq!(6, find_tetrahedral_number_base(8));
+        assert_eq!(
+            505013002528,
+            find_tetrahedral_number_base(169179692512835000)
+        );
     }
 }
