b78fd06230403aa273410a0924f61bad2c7d3ec8
CJ Harries <cj@wizardsoftheweb.pro>
2023-02-12 11:56:13 -0600

    Test find_complexity

--- intermediate/031/rust/src/main.rs
+++ intermediate/031/rust/src/main.rs
@@ -28,6 +28,18 @@ mod tests {
 
     #[test]
     fn test_stub() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(1, find_complexity(1));
+        assert_eq!(2, find_complexity(2));
+        assert_eq!(3, find_complexity(3));
+        assert_eq!(4, find_complexity(4));
+        assert_eq!(5, find_complexity(5));
+        assert_eq!(5, find_complexity(6));
+        assert_eq!(6, find_complexity(7));
+        assert_eq!(6, find_complexity(8));
+        assert_eq!(6, find_complexity(9));
+        assert_eq!(7, find_complexity(10));
+        assert_eq!(8, find_complexity(11));
+        assert_eq!(7, find_complexity(12));
+        assert_eq!(27, find_complexity(6161));
     }
 }
