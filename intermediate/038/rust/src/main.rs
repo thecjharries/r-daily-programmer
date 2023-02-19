537f8470b9fb34dd5f152286f191a2d752b4ffe5
CJ Harries <cj@wizardsoftheweb.pro>
2023-02-19 12:17:39 -0600

    Build const slice of operators

--- intermediate/038/rust/src/main.rs
+++ intermediate/038/rust/src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const OPERATORS: [char; 5] = ['+', '-', '*', '/', '^'];
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
