8ac93a22d7d5d1e9e7177182348804d4e1514cd2
CJ Harries <cj@wizardsoftheweb.pro>
2023-02-26 09:35:33 -0600

    Swap 044 <-> 045 (whoops)

--- intermediate/044/rust/src/main.rs
+++ intermediate/044/rust/src/main.rs
@@ -12,36 +12,93 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::collections::{BTreeMap, HashMap};
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum GameState {
+    Playing,
+    Win,
+    Lose,
+    Draw,
+}
 
-lazy_static! {
-    static ref WORD_PATTERN: Regex = Regex::new(r"\w+").unwrap();
+#[derive(Debug, PartialEq, Clone, Copy)]
+#[repr(u8)]
+enum Move {
+    First = b'X',
+    Second = b'O',
+}
+
+impl Into<char> for Move {
+    fn into(self) -> char {
+        self as u8 as char
+    }
+}
+
+impl From<char> for Move {
+    fn from(character: char) -> Self {
+        match character {
+            'X' => Move::First,
+            'O' => Move::Second,
+            _ => panic!("Invalid move"),
+        }
+    }
+}
+
+struct TicTacToe {
+    grid: Vec<char>,
+    state: GameState,
+    player_move_next: bool,
+}
+
+impl Default for TicTacToe {
+    fn default() -> Self {
+        TicTacToe {
+            grid: vec![' '; 9],
+            state: GameState::Playing,
+            player_move_next: true,
+        }
+    }
+}
+
+impl TicTacToe {
+    fn turn(&mut self, x: usize, y: usize, character: char) {
+        let move_type = Move::from(character);
+        self.grid[x + y * 3] = move_type.into();
+        self.player_move_next = !self.player_move_next;
+        self.check_state();
+    }
+
+    fn check_state(&mut self) {
+        let winning_combinations = vec![
+            vec![0, 1, 2],
+            vec![3, 4, 5],
+            vec![6, 7, 8],
+            vec![0, 3, 6],
+            vec![1, 4, 7],
+            vec![2, 5, 8],
+            vec![0, 4, 8],
+            vec![2, 4, 6],
+        ];
+        for combination in winning_combinations.iter() {
+            if self.grid[combination[0]] == self.grid[combination[1]]
+                && self.grid[combination[1]] == self.grid[combination[2]]
+                && self.grid[combination[0]] != ' '
+            {
+                self.state = if self.player_move_next {
+                    GameState::Lose
+                } else {
+                    GameState::Win
+                };
+            }
+        }
+        if self.grid.iter().all(|&character| character != ' ') {
+            self.state = GameState::Draw;
+        }
+    }
 }
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
-}
-
-fn find_hapaxes(input: &str) -> Vec<String> {
-    let mut word_counts: HashMap<String, u32> = HashMap::new();
-    for word in WORD_PATTERN.find_iter(input) {
-        let word = word.as_str().to_lowercase();
-        let count = word_counts.entry(word).or_insert(0);
-        *count += 1;
-    }
-    let mut word_lengths: BTreeMap<u32, Vec<String>> = BTreeMap::new();
-    for (word, count) in word_counts {
-        if 1 == count {
-            let length = word.len() as u32;
-            let words = word_lengths.entry(length).or_insert(vec![]);
-            words.push(word);
-        }
-    }
-    let max_length = word_lengths.keys().max().unwrap_or(&0);
-    word_lengths.get(max_length).unwrap_or(&vec![]).clone()
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -50,18 +107,64 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_find_hapaxes() {
-        assert_eq!(
-            vec![String::from("correspondence")],
-            find_hapaxes(
-                "
+    fn test_game_default() {
+        let game = TicTacToe::default();
+        assert_eq!(vec![' '; 9], game.grid);
+        assert_eq!(GameState::Playing, game.state);
+        assert_eq!(true, game.player_move_next);
+    }
 
-        O me! what eyes hath love put in my head,
-        Which have no correspondence with true sight,
-        Or if they have, where is my judgment fled,
-        That censures falsely what they see aright?
-    "
-            )
-        );
+    #[test]
+    fn test_move_as_char() {
+        assert_eq!('X', Move::First.into());
+        assert_eq!('O', Move::Second.into());
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid move")]
+    fn test_char_as_move() {
+        assert_eq!(Move::First, Move::from('X'));
+        assert_eq!(Move::Second, Move::from('O'));
+        assert!(Move::from('Z') == Move::First);
+    }
+
+    #[test]
+    fn test_player_win() {
+        let mut game = TicTacToe::default();
+        game.player_move_next = false;
+        game.grid = vec!['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' '];
+        game.check_state();
+        assert_eq!(GameState::Win, game.state);
+    }
+
+    #[test]
+    fn test_player_lose() {
+        let mut game = TicTacToe::default();
+        game.grid = vec!['O', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' '];
+        game.check_state();
+        assert_eq!(GameState::Lose, game.state);
+    }
+
+    #[test]
+    fn test_game_draw() {
+        let mut game = TicTacToe::default();
+        game.grid = vec!['X', 'O', 'X', 'O', 'X', 'O', 'O', 'X', 'O'];
+        game.check_state();
+        assert_eq!(GameState::Draw, game.state);
+    }
+
+    #[test]
+    fn test_game_playing() {
+        let mut game = TicTacToe::default();
+        game.grid = vec!['X', 'O', 'X', 'O', 'X', 'O', 'O', 'X', ' '];
+        game.check_state();
+        assert_eq!(GameState::Playing, game.state);
+    }
+
+    #[test]
+    fn test_game_turn() {
+        let mut game = TicTacToe::default();
+        game.turn(0, 0, 'X');
+        assert_eq!(vec!['X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], game.grid);
     }
 }
