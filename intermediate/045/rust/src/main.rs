e95c31804334b239eb439ca0fabf41755dddd0ab
CJ Harries <cj@wizardsoftheweb.pro>
2023-02-26 09:18:20 -0600

    Create enum for game state

--- intermediate/045/rust/src/main.rs
+++ intermediate/045/rust/src/main.rs
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+enum GameState {
+    Playing
+    Win,
+    Lose,
+    Draw,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
