48fc5bb5067adbdd34960613381d9cceb89496b2
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-13 13:34:30 -0500

    Implement parse_and_sum with custom error type

--- intermediate/055/rust/src/main.rs
+++ intermediate/055/rust/src/main.rs
@@ -12,15 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::num::ParseIntError;
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ParseError {
+    position: usize,
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid number in position {}", self.position)
+    }
+}
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
 }
 
-fn parse_and_sum(first: char, second: char) -> Result<u32, ParseIntError> {
-    todo!()
+fn parse_and_sum(first: char, second: char) -> Result<u32, ParseError> {
+    let first_number = match first.to_digit(10) {
+        Some(number) => number,
+        None => return Err(ParseError { position: 1 }),
+    };
+    let second_number = match second.to_digit(10) {
+        Some(number) => number,
+        None => return Err(ParseError { position: 2 }),
+    };
+    Ok(first_number + second_number)
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -34,6 +53,8 @@ mod tests {
         assert_eq!(Ok(13), parse_and_sum('4', '9'));
         assert_eq!(Ok(9), parse_and_sum('0', '9'));
         assert!(parse_and_sum('g', '6').is_err());
+        let error = parse_and_sum('7', 'h').unwrap_err();
+        assert_eq!(2, error.position);
         assert!(parse_and_sum('7', 'h').is_err());
     }
 }
