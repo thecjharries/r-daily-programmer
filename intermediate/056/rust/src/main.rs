07326bf8b4440f9a0d9a2efe80e7106d8fdf593d
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-14 15:33:33 -0500

    Implement has_vowels_zero_mod_three without regex

--- intermediate/056/rust/src/main.rs
+++ intermediate/056/rust/src/main.rs
@@ -19,9 +19,20 @@ fn main() {
     println!("rad");
 }
 
-fn has_vowels_zero_mod_three(input: &str) -> bool {
+fn has_vowels_zero_mod_three_regex(input: &str) -> bool {
     let vowels = Regex::new(r"[aeiouy]").unwrap();
     vowels.find_iter(input).count() % 3 == 0
+}
+
+fn has_vowels_zero_mod_three_match(input: &str) -> bool {
+    let mut vowel_count = 0;
+    for character in input.chars() {
+        match character {
+            'a' | 'e' | 'i' | 'o' | 'u' | 'y' => vowel_count += 1,
+            _ => {}
+        }
+    }
+    vowel_count % 3 == 0
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -30,16 +41,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_has_vowels_zero_mod_three() {
-        assert!(has_vowels_zero_mod_three(
+    fn test_has_vowels_zero_mod_three_regex() {
+        assert!(has_vowels_zero_mod_three_regex(
             "Friends, Romans, countrymen, lend me your ears!"
         ));
-        assert!(has_vowels_zero_mod_three(
+        assert!(has_vowels_zero_mod_three_regex(
             "Double, double, toil and trouble; Fire burn and cauldron bubble."
         ));
-        assert!(!has_vowels_zero_mod_three("Alas, poor Yorick! I knew him, Horatio. A fellow of infinite jest, of most excellent fancy."));
-        assert!(!has_vowels_zero_mod_three("To be, or not to be- that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them."));
-        assert!(!has_vowels_zero_mod_three(
+        assert!(!has_vowels_zero_mod_three_regex("Alas, poor Yorick! I knew him, Horatio. A fellow of infinite jest, of most excellent fancy."));
+        assert!(!has_vowels_zero_mod_three_regex("To be, or not to be- that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them."));
+        assert!(!has_vowels_zero_mod_three_regex(
+            "Everybody stand back! I know regular expressions."
+        ));
+    }
+
+    #[test]
+    fn test_has_vowels_zero_mod_three_match() {
+        assert!(has_vowels_zero_mod_three_match(
+            "Friends, Romans, countrymen, lend me your ears!"
+        ));
+        assert!(has_vowels_zero_mod_three_match(
+            "Double, double, toil and trouble; Fire burn and cauldron bubble."
+        ));
+        assert!(!has_vowels_zero_mod_three_match("Alas, poor Yorick! I knew him, Horatio. A fellow of infinite jest, of most excellent fancy."));
+        assert!(!has_vowels_zero_mod_three_match("To be, or not to be- that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them."));
+        assert!(!has_vowels_zero_mod_three_match(
             "Everybody stand back! I know regular expressions."
         ));
     }
