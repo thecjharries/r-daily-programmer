94c59c78dc3bb09dc3c84a5f5bb8426ebff00071
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-15 17:10:17 -0500

    Test InvertedMatchTable::from_str

--- intermediate/057/rust/src/main.rs
+++ intermediate/057/rust/src/main.rs
@@ -15,9 +15,10 @@
 use std::collections::HashMap;
 use std::str::FromStr;
 
+#[derive(Debug, PartialEq, Eq)]
 struct InvertedMatchTable {
-    rows: HashMap<char, u8>,
-    columns: HashMap<char, u8>,
+    rows: HashMap<char, HashMap<char, u8>>,
+    columns: HashMap<char, HashMap<char, u8>>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -45,7 +46,57 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_invertedmatchtable_from_str() {
+        //  ABC
+        // A010
+        // B111
+        // C011
+        let result = InvertedMatchTable {
+            rows: HashMap::from_iter(vec![
+                ('A', HashMap::from_iter(vec![('A', 0), ('B', 1), ('C', 0)])),
+                ('B', HashMap::from_iter(vec![('A', 1), ('B', 1), ('C', 1)])),
+                ('C', HashMap::from_iter(vec![('A', 0), ('B', 1), ('C', 1)])),
+            ]),
+            columns: HashMap::from_iter(vec![
+                ('A', HashMap::from_iter(vec![('A', 0), ('B', 1), ('C', 0)])),
+                ('B', HashMap::from_iter(vec![('A', 1), ('B', 1), ('C', 1)])),
+                ('C', HashMap::from_iter(vec![('A', 0), ('B', 1), ('C', 1)])),
+            ]),
+        };
+        assert_eq!(
+            result,
+            InvertedMatchTable::from_str(" ABC\nA010\nB111\nC011").unwrap()
+        );
+        //  ABCDEFGHIJKLMNOPQRST
+        // A11110101111011100010
+        // B10010010000010001100
+        // C01101110010001000000
+        // D10110011001011101100
+        // E10100100011110110100
+        // F01111011000111010010
+        // G00011110001011001110
+        // H01111000010001001000
+        // I01101110010110010011
+        // J00101000100010011110
+        // K10101001100001100000
+        // L01011010011101100110
+        // M10110110010101000100
+        // N10001111101111110010
+        // O11011010010111100110
+        // P01000110111101101000
+        // Q10011001100010100000
+        // R11101011100110110110
+        // S00001100000110010101
+        // T01000110011100101011
+        let big_table = InvertedMatchTable::from_str(" ABCDEFGHIJKLMNOPQRST\nA11110101111011100010\nB10010010000010001100\nC01101110010001000000\nD10110011001011101100\nE10100100011110110100\nF01111011000111010010\nG00011110001011001110\nH01111000010001001000\nI01101110010110010011\nJ00101000100010011110\nK10101001100001100000\nL01011010011101100110\nM10110110010101000100\nN10001111101111110010\nO11011010010111100110\nP01000110111101101000\nQ10011001100010100000\nR11101011100110110110\nS00001100000110010101\nT01000110011100101011").unwrap();
+        let a_row = big_table.rows.get(&'A').unwrap();
+        assert_eq!(1, *a_row.get(&'A').unwrap());
+        assert_eq!(1, *a_row.get(&'B').unwrap());
+        assert_eq!(1, *a_row.get(&'C').unwrap());
+        assert_eq!(1, *a_row.get(&'D').unwrap());
+        assert_eq!(0, *a_row.get(&'E').unwrap());
+        assert_eq!(1, *a_row.get(&'F').unwrap());
+        assert_eq!(0, *a_row.get(&'G').unwrap());
+        assert_eq!(1, *a_row.get(&'H').unwrap());
     }
 }
