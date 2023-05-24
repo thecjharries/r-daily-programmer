06678fb3593224b8a49379920ce40cc8e13cc7fb
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-24 16:15:08 -0500

    Define MaxiphobicMap

--- intermediate/066/rust/src/main.rs
+++ intermediate/066/rust/src/main.rs
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::BTreeMap;
+
+struct MaxiphobicMap<T>(BTreeMap<String, T>);
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
