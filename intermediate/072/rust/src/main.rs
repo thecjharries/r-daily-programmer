b283b6e9d0579144692f93adb762fb719058a06d
CJ Harries <cj@wizardsoftheweb.pro>
2023-05-30 17:46:20 -0500

    Define Coordinate struct

--- intermediate/072/rust/src/main.rs
+++ intermediate/072/rust/src/main.rs
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug, PartialEq, Eq)]
+struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug, PartialEq, Eq)]
 struct Xray(Vec<Vec<char>>);
 
 impl Xray {
