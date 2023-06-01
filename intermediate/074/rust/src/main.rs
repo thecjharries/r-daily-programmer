e9af4a88e8f5e2d4105836cc81d77f4ca421195d
CJ Harries <cj@wizardsoftheweb.pro>
2023-06-01 10:17:23 -0500

    Implement and basic test DancingLinks::new

--- intermediate/074/rust/src/main.rs
+++ intermediate/074/rust/src/main.rs
@@ -45,10 +45,61 @@ impl Cell {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct DancingLinks {
     matrix: Vec<Vec<bool>>,
     cells: Vec<Cell>,
+}
+
+impl DancingLinks {
+    fn new(matrix: Vec<Vec<bool>>) -> Self {
+        let mut cells: Vec<Cell> = Vec::new();
+        for (y, row) in matrix.iter().enumerate() {
+            for (x, value) in row.iter().enumerate() {
+                if *value {
+                    let mut cell = Cell::new(Coordinate::new(x, y));
+                    let mut has_up = false;
+                    for up in (0..y).rev() {
+                        if matrix[up][x] {
+                            cell.up = Coordinate::new(x, up);
+                            cell.down = Coordinate::new(x, up);
+                            has_up = true;
+                            break;
+                        }
+                    }
+                    for down in (y + 1)..matrix.len() {
+                        if matrix[down][x] {
+                            cell.down = Coordinate::new(x, down);
+                            if !has_up {
+                                cell.up = Coordinate::new(x, down);
+                            }
+                            break;
+                        }
+                    }
+                    let mut has_left = false;
+                    for left in (0..x).rev() {
+                        if matrix[y][left] {
+                            cell.left = Coordinate::new(left, y);
+                            cell.right = Coordinate::new(left, y);
+                            has_left = true;
+                            break;
+                        }
+                    }
+                    for right in (x + 1)..row.len() {
+                        if matrix[y][right] {
+                            cell.right = Coordinate::new(right, y);
+                            if !has_left {
+                                cell.left = Coordinate::new(right, y);
+                            }
+                            break;
+                        }
+                    }
+                    cells.push(cell);
+                }
+            }
+        }
+        DancingLinks { matrix, cells }
+    }
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -77,5 +128,17 @@ mod tests {
         assert_eq!(coordinate, cell.down);
         assert_eq!(coordinate, cell.left);
         assert_eq!(coordinate, cell.right);
+    }
+
+    #[test]
+    fn test_dancinglinks_new() {
+        let matrix = vec![
+            vec![false, false, false],
+            vec![false, true, false],
+            vec![false, false, false],
+        ];
+        let dancing_links = DancingLinks::new(matrix.clone());
+        assert_eq!(matrix, dancing_links.matrix);
+        assert_eq!(vec![Cell::new(Coordinate::new(1, 1))], dancing_links.cells);
     }
 }
