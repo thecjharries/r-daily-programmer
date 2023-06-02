0f27d87419320116e0c05a8f2f8a4d90d5f696d7
CJ Harries <cj@wizardsoftheweb.pro>
2023-06-02 09:38:42 -0500

    Create InlineGroup and impl+test FromStr on InlineGroup

--- intermediate/075/rust/src/main.rs
+++ intermediate/075/rust/src/main.rs
@@ -32,6 +32,23 @@ impl FromStr for BuildType {
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct InlineGroup {
+    keyword: String,
+    values: Vec<String>,
+}
+
+impl FromStr for InlineGroup {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut split = input.split_whitespace();
+        let keyword = split.next().unwrap().to_string();
+        let values = split.map(|value| value.to_string()).collect();
+        Ok(InlineGroup { keyword, values })
+    }
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
@@ -47,5 +64,14 @@ mod tests {
         assert_eq!(Ok(BuildType::Exe), BuildType::from_str("exe"));
         assert_eq!(Ok(BuildType::Lib), BuildType::from_str("lib"));
         assert_eq!(Err(()), BuildType::from_str("rad"));
+    }
+
+    #[test]
+    fn test_inlinegroup_from_str() {
+        let expected = InlineGroup {
+            keyword: "cflags".to_string(),
+            values: vec!["-Wall".to_string(), "-Wextra".to_string()],
+        };
+        assert_eq!(Ok(expected), InlineGroup::from_str("cflags -Wall -Wextra"));
     }
 }
