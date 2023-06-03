759d443f7c9b68c1c00d6989b4991b671623c406
CJ Harries <cj@wizardsoftheweb.pro>
2023-06-03 07:40:25 -0500

    Stub prompt fnc

--- intermediate/076/rust/src/main.rs
+++ intermediate/076/rust/src/main.rs
@@ -12,9 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use rand::prelude::*;
+use rand::Rng;
+use rand_pcg::Pcg64;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
+}
+
+fn graph(f: &dyn FnMut(u32) -> u32, count: u32) -> Vec<String> {
+    todo!()
 }
 
 #[cfg(not(tarpaulin_include))]
