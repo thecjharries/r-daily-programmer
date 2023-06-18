88cfdbfe35674b9124878c42546c07d1023d79fa
CJ Harries <cj@wizardsoftheweb.pro>
2023-06-18 15:54:18 -0500

    Define Fraction struct

--- intermediate/091/rust/src/main.rs
+++ intermediate/091/rust/src/main.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+struct Fraction {
+    numerator: u64,
+    denominator: u64,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
