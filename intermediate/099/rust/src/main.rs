030da90a3efb773bb27f4f7b2ee03d8d402b5d32
CJ Harries <cj@wizardsoftheweb.pro>
2023-06-26 14:51:20 -0500

    Build struct for single row record

--- intermediate/099/rust/src/main.rs
+++ intermediate/099/rust/src/main.rs
@@ -12,6 +12,114 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use chrono::NaiveDate;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    date: NaiveDate,
+    #[serde(rename = "AK")]
+    alaska: Option<f32>,
+    #[serde(rename = "AL")]
+    alabama: Option<f32>,
+    #[serde(rename = "AR")]
+    arizona: Option<f32>,
+    #[serde(rename = "AZ")]
+    arkansas: Option<f32>,
+    #[serde(rename = "CA")]
+    california: Option<f32>,
+    #[serde(rename = "CO")]
+    colorado: Option<f32>,
+    #[serde(rename = "CT")]
+    connecticut: Option<f32>,
+    #[serde(rename = "DE")]
+    delaware: Option<f32>,
+    #[serde(rename = "FL")]
+    florida: Option<f32>,
+    #[serde(rename = "GA")]
+    georgia: Option<f32>,
+    #[serde(rename = "HI")]
+    hawaii: Option<f32>,
+    #[serde(rename = "IA")]
+    iowa: Option<f32>,
+    #[serde(rename = "ID")]
+    idaho: Option<f32>,
+    #[serde(rename = "IL")]
+    illinois: Option<f32>,
+    #[serde(rename = "IN")]
+    indiana: Option<f32>,
+    #[serde(rename = "KS")]
+    kansas: Option<f32>,
+    #[serde(rename = "KY")]
+    kentucky: Option<f32>,
+    #[serde(rename = "LA")]
+    louisiana: Option<f32>,
+    #[serde(rename = "MA")]
+    massachusetts: Option<f32>,
+    #[serde(rename = "MD")]
+    maryland: Option<f32>,
+    #[serde(rename = "ME")]
+    maine: Option<f32>,
+    #[serde(rename = "MI")]
+    michigan: Option<f32>,
+    #[serde(rename = "MN")]
+    minnesota: Option<f32>,
+    #[serde(rename = "MO")]
+    missouri: Option<f32>,
+    #[serde(rename = "MS")]
+    mississippi: Option<f32>,
+    #[serde(rename = "MT")]
+    montana: Option<f32>,
+    #[serde(rename = "NC")]
+    north_carolina: Option<f32>,
+    #[serde(rename = "ND")]
+    north_dakota: Option<f32>,
+    #[serde(rename = "NE")]
+    nebraska: Option<f32>,
+    #[serde(rename = "NH")]
+    new_hampshire: Option<f32>,
+    #[serde(rename = "NJ")]
+    new_jersey: Option<f32>,
+    #[serde(rename = "NM")]
+    new_mexico: Option<f32>,
+    #[serde(rename = "NV")]
+    nevada: Option<f32>,
+    #[serde(rename = "NY")]
+    new_york: Option<f32>,
+    #[serde(rename = "OH")]
+    ohio: Option<f32>,
+    #[serde(rename = "OK")]
+    oklahoma: Option<f32>,
+    #[serde(rename = "OR")]
+    oregon: Option<f32>,
+    #[serde(rename = "PA")]
+    pennsylvania: Option<f32>,
+    #[serde(rename = "RI")]
+    rhode_island: Option<f32>,
+    #[serde(rename = "SC")]
+    south_carolina: Option<f32>,
+    #[serde(rename = "SD")]
+    south_dakota: Option<f32>,
+    #[serde(rename = "TN")]
+    tennessee: Option<f32>,
+    #[serde(rename = "TX")]
+    texas: Option<f32>,
+    #[serde(rename = "UT")]
+    utah: Option<f32>,
+    #[serde(rename = "VA")]
+    virginia: Option<f32>,
+    #[serde(rename = "VT")]
+    vermont: Option<f32>,
+    #[serde(rename = "WA")]
+    washington: Option<f32>,
+    #[serde(rename = "WI")]
+    wisconsin: Option<f32>,
+    #[serde(rename = "WV")]
+    west_virginia: Option<f32>,
+    #[serde(rename = "WY")]
+    wyoming: Option<f32>,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
