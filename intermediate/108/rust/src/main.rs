d2a25735aea933b6be084721b3618a7293359e12
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-22 08:39:46 -0500

    Create enum for tile type

--- intermediate/108/rust/src/main.rs
+++ intermediate/108/rust/src/main.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug, PartialEq)]
+enum Tile {
+    Neighbor(u8),
+    Mine,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
