859d9187f6a35938c976121b770a01885c18625a
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-24 08:15:52 -0500

    Define sudoku type

--- intermediate/111/rust/src/main.rs
+++ intermediate/111/rust/src/main.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug, PartialEq)]
+struct Sudoku([u8; 81]);
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
