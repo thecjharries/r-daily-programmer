b4ce1587a24868cda63640c70c58cda748fe913b
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-25 09:34:48 -0500

    Add chrono dep

--- intermediate/112/rust/src/main.rs
+++ intermediate/112/rust/src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use chrono::NaiveDateTime;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
