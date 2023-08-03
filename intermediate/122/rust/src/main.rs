39599b10ef545c93b782f811753de18424ff7de4
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-03 14:51:57 -0500

    Add thread calls to main

--- intermediate/122/rust/src/main.rs
+++ intermediate/122/rust/src/main.rs
@@ -12,9 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::thread;
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
-    println!("rad");
+    println!("main rad");
+    thread::spawn(|| {
+        println!("thread rad");
+    });
 }
 
 #[cfg(not(tarpaulin_include))]
