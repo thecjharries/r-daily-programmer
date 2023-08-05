f5f567cb64b92c43de85a19efab3af44b25cd11a
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-05 08:28:44 -0500

    Define graph struct

--- intermediate/124/rust/src/main.rs
+++ intermediate/124/rust/src/main.rs
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::BTreeMap;
+
+#[derive(Debug)]
+struct DirectedGraph {
+    edges: BTreeMap<u32, Vec<u32>>,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
