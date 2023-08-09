47f857bd9598ce216342b10110727cfeea102ae0
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-09 09:50:51 -0500

    Create road struct

--- intermediate/128/rust/src/main.rs
+++ intermediate/128/rust/src/main.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug)]
+struct Roads(Vec<Vec<u8>>);
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
