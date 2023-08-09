54556e0914a698969507bb13706cbb3151517db6
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-09 09:59:11 -0500

    Create and test Roads::find_most_potholes

--- intermediate/128/rust/src/main.rs
+++ intermediate/128/rust/src/main.rs
@@ -13,11 +13,40 @@
 // limitations under the License.
 
 #[derive(Debug, PartialEq)]
+enum Direction {
+    Row,
+    Column,
+}
+
+#[derive(Debug, PartialEq)]
 struct Roads(Vec<Vec<u8>>);
 
 impl Roads {
     fn new(roads: Vec<Vec<u8>>) -> Self {
         Self(roads)
+    }
+
+    fn find_most_potholes(&self) -> Option<(Direction, usize)> {
+        let mut result = None;
+        let mut most_potholes = 0;
+        for (index, row) in self.0.iter().enumerate() {
+            let row_potholes = row.iter().filter(|&&x| x == 0).count();
+            if row_potholes > most_potholes {
+                most_potholes = row_potholes;
+                result = Some((Direction::Row, index));
+            }
+            let column_potholes = self
+                .0
+                .iter()
+                .map(|row| row[index])
+                .filter(|&x| x == 0)
+                .count();
+            if column_potholes > most_potholes {
+                most_potholes = column_potholes;
+                result = Some((Direction::Column, index));
+            }
+        }
+        result
     }
 }
 
@@ -50,5 +79,21 @@ mod tests {
                 vec![0, 1, 0, 1, 0],
             ])
         );
+    }
+
+    #[test]
+    fn road_find_most_potholes_identifies_the_largest_row_or_column() {
+        let roads = Roads::new(vec![
+            vec![0, 1, 0, 1, 0],
+            vec![1, 0, 0, 0, 1],
+            vec![1, 1, 1, 1, 1],
+            vec![1, 0, 0, 0, 1],
+            vec![0, 1, 0, 1, 0],
+        ]);
+        assert_eq!(Some((Direction::Column, 2)), roads.find_most_potholes());
+        let roads = Roads::new(vec![vec![1, 1], vec![1, 1]]);
+        assert!(roads.find_most_potholes().is_none());
+        let roads = Roads::new(vec![vec![0, 0], vec![0, 0]]);
+        assert_eq!(Some((Direction::Row, 0)), roads.find_most_potholes());
     }
 }
