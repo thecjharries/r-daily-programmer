9d6a1c95ba8f703365a4773c2faea6fead020eda
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-10 10:51:56 -0500

    Define type

--- intermediate/129/rust/src/main.rs
+++ intermediate/129/rust/src/main.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug, PartialEq, Clone)]
+struct NRealNumber(Vec<f64>);
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
