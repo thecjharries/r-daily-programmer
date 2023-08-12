9e3cd093dd1d703402980f85a06705ffc2fd92ae
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-12 07:40:03 -0500

    Create grid representation

--- intermediate/131/rust/src/main.rs
+++ intermediate/131/rust/src/main.rs
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug,PartialEq)]
+struct Grid {
+    width: usize,
+    height: usize,
+    cells: Vec<Vec<bool>>,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
