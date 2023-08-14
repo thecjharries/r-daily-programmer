cdb906ae8c9e716f4f18e5a6853e6f5f799be32a
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-14 08:37:44 -0500

    Create direction enum

--- intermediate/133/rust/src/main.rs
+++ intermediate/133/rust/src/main.rs
@@ -12,6 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
