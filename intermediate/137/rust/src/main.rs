32812ed297b81bdd9778725dd74d74a542dff840
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-17 07:54:55 -0500

    Create struct for food items

--- intermediate/137/rust/src/main.rs
+++ intermediate/137/rust/src/main.rs
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug, PartialEq)]
+struct FoodItem {
+    name: String,
+    potential_index: usize,
+    after: Vec<String>,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
