5c0b762a6036a103e5f64a73a1180eeff6703e5a
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-20 10:51:46 -0500

    Stub Floyd Marshall algo fnc

--- intermediate/140/rust/src/main.rs
+++ intermediate/140/rust/src/main.rs
@@ -17,6 +17,10 @@ fn main() {
     println!("rad");
 }
 
+fn floyd_marshall(adjacency: Vec<Vec<bool>>) -> Vec<Vec<usize>> {
+    todo!()
+}
+
 #[cfg(not(tarpaulin_include))]
 #[cfg(test)]
 mod tests {
