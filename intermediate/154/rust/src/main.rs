602f481f843354bb5ac0375dc2d1d6fdeeae1004
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-23 07:16:10 -0500

    Create Alphabet struct

--- intermediate/154/rust/src/main.rs
+++ intermediate/154/rust/src/main.rs
@@ -12,6 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug, PartialEq)]
+struct Alphabet {
+    letters: Vec<char>,
+}
+
 #[cfg(not(tarpaulin_include))]
 fn main() {
     println!("rad");
