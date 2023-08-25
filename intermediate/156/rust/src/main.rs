53e2a0352f58ceb675b3e63a47e7f22150ef71d2
CJ Harries <cj@wizardsoftheweb.pro>
2023-08-25 07:55:36 -0500

    Test build_work_list

--- intermediate/156/rust/src/main.rs
+++ intermediate/156/rust/src/main.rs
@@ -29,7 +29,56 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn build_work_list_naively_loads_many_workers() {
+        let input = vec![
+            ("Lights", 2, Some("Wiring")),
+            ("Windows", 3, None),
+            ("Insulation", 4, None),
+            ("Painting", 4, None),
+            ("Wiring", 6, None),
+            ("Cleaning", 7, Some("Painting")),
+        ];
+        let expected = vec![
+            (1, "Painting".to_string(), 4),
+            (1, "Cleaning".to_string(), 7),
+            (2, "Insulation".to_string(), 4),
+            (3, "Wiring".to_string(), 6),
+            (3, "Lights".to_string(), 2),
+            (4, "Windows".to_string(), 3),
+        ];
+        let idle = 18;
+        assert_eq!((expected, idle), build_work_list(input));
+        let input = vec![
+            ("Preparation", 2, Some("Planning")),
+            ("Hiring", 3, None),
+            ("Legal", 3, None),
+            ("Briefing", 4, Some("Preparation")),
+            ("Advertising", 4, None),
+            ("Paperwork", 5, Some("Legal")),
+            ("Testing", 5, Some("Frontend")),
+            ("API", 6, None),
+            ("Backend", 6, None),
+            ("Planning", 7, None),
+            ("Frontend", 8, None),
+            ("Mobile", 8, None),
+            ("Documentation", 9, Some("API")),
+        ];
+        let expected = vec![
+            (1, "Advertising".to_string(), 4),
+            (2, "Backend".to_string(), 6),
+            (3, "Planning".to_string(), 7),
+            (3, "Preparation".to_string(), 2),
+            (3, "Briefing".to_string(), 4),
+            (4, "API".to_string(), 6),
+            (4, "Documentation".to_string(), 9),
+            (5, "Hiring".to_string(), 3),
+            (6, "Mobile".to_string(), 8),
+            (7, "Legal".to_string(), 3),
+            (7, "Paperwork".to_string(), 5),
+            (8, "Frontend".to_string(), 8),
+            (8, "Testing".to_string(), 5),
+        ];
+        let idle = 50;
+        assert_eq!((expected, idle), build_work_list(input));
     }
 }
