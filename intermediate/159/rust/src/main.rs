caad105ea5c1ac68895c3ed895e77c4a00788bf1
CJ Harries <cj@wizardsoftheweb.pro>
2023-10-17 07:54:12 -0500

    Define winning outcomes

--- intermediate/159/rust/src/main.rs
+++ intermediate/159/rust/src/main.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+enum Winner {
+    Human,
+    Computer,
+    Neither,
+}
+
 enum Moves {
     Rock,
     Paper,
