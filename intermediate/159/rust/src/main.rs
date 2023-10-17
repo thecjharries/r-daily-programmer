eb09423eda9de2f52e4958fbc83c6dae133e5857
CJ Harries <cj@wizardsoftheweb.pro>
2023-10-17 07:59:41 -0500

    Test all win conditions

--- intermediate/159/rust/src/main.rs
+++ intermediate/159/rust/src/main.rs
@@ -12,18 +12,57 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[derive(Debug, PartialEq)]
 enum Winner {
     Human,
     Computer,
     Neither,
 }
 
+#[derive(Debug, PartialEq)]
 enum Moves {
     Rock,
     Paper,
     Scissors,
     Lizard,
     Spock,
+}
+
+impl Moves {
+    fn get_winner_as_human(&self, other: &Self) -> Winner {
+        match self {
+            Self::Rock => match other {
+                Self::Rock => Winner::Neither,
+                Self::Scissors => Winner::Human,
+                Self::Lizard => Winner::Human,
+                _ => Winner::Computer,
+            },
+            Self::Paper => match other {
+                Self::Paper => Winner::Neither,
+                Self::Rock => Winner::Human,
+                Self::Spock => Winner::Human,
+                _ => Winner::Computer,
+            },
+            Self::Scissors => match other {
+                Self::Scissors => Winner::Neither,
+                Self::Paper => Winner::Human,
+                Self::Lizard => Winner::Human,
+                _ => Winner::Computer,
+            },
+            Self::Lizard => match other {
+                Self::Lizard => Winner::Neither,
+                Self::Paper => Winner::Human,
+                Self::Spock => Winner::Human,
+                _ => Winner::Computer,
+            },
+            Self::Spock => match other {
+                Self::Spock => Winner::Neither,
+                Self::Rock => Winner::Human,
+                Self::Scissors => Winner::Human,
+                _ => Winner::Computer,
+            },
+        }
+    }
 }
 
 #[cfg(not(tarpaulin_include))]
@@ -37,7 +76,106 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_stub() {
-        assert_eq!(2 + 2, 4);
+    fn test_win_conditions() {
+        assert_eq!(
+            Winner::Human,
+            Moves::Rock.get_winner_as_human(&Moves::Scissors)
+        );
+        assert_eq!(
+            Winner::Human,
+            Moves::Rock.get_winner_as_human(&Moves::Lizard)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Rock.get_winner_as_human(&Moves::Paper)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Rock.get_winner_as_human(&Moves::Spock)
+        );
+        assert_eq!(
+            Winner::Neither,
+            Moves::Rock.get_winner_as_human(&Moves::Rock)
+        );
+        assert_eq!(
+            Winner::Human,
+            Moves::Paper.get_winner_as_human(&Moves::Rock)
+        );
+        assert_eq!(
+            Winner::Human,
+            Moves::Paper.get_winner_as_human(&Moves::Spock)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Paper.get_winner_as_human(&Moves::Scissors)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Paper.get_winner_as_human(&Moves::Lizard)
+        );
+        assert_eq!(
+            Winner::Neither,
+            Moves::Paper.get_winner_as_human(&Moves::Paper)
+        );
+        assert_eq!(
+            Winner::Human,
+            Moves::Scissors.get_winner_as_human(&Moves::Paper)
+        );
+        assert_eq!(
+            Winner::Human,
+            Moves::Scissors.get_winner_as_human(&Moves::Lizard)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Scissors.get_winner_as_human(&Moves::Rock)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Scissors.get_winner_as_human(&Moves::Spock)
+        );
+        assert_eq!(
+            Winner::Neither,
+            Moves::Scissors.get_winner_as_human(&Moves::Scissors)
+        );
+        assert_eq!(
+            Winner::Human,
+            Moves::Lizard.get_winner_as_human(&Moves::Paper)
+        );
+        assert_eq!(
+            Winner::Human,
+            Moves::Lizard.get_winner_as_human(&Moves::Spock)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Lizard.get_winner_as_human(&Moves::Rock)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Lizard.get_winner_as_human(&Moves::Scissors)
+        );
+        assert_eq!(
+            Winner::Neither,
+            Moves::Lizard.get_winner_as_human(&Moves::Lizard)
+        );
+        assert_eq!(
+            Winner::Human,
+            Moves::Spock.get_winner_as_human(&Moves::Rock)
+        );
+        assert_eq!(
+            Winner::Human,
+            Moves::Spock.get_winner_as_human(&Moves::Scissors)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Spock.get_winner_as_human(&Moves::Paper)
+        );
+        assert_eq!(
+            Winner::Computer,
+            Moves::Spock.get_winner_as_human(&Moves::Lizard)
+        );
+        assert_eq!(
+            Winner::Neither,
+            Moves::Spock.get_winner_as_human(&Moves::Spock)
+        );
     }
 }
